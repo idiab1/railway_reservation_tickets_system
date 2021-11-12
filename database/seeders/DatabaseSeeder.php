f03e5b7f14ecc941313d9262eea71e28b1486dc8
islamdiab-stack <islamdiab967@gmail.com>
2021-11-12 03:41:50 +0200

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(LaratrustSeeder::class);
+        $this->call([
+            LaratrustSeeder::class,
+            UsersTableSeeder::class
+        ]);
 
     }
 }
