5e67642942e769d532e2ef7948a3e7dca0ea3537
islamdiab-stack <islamdiab967@gmail.com>
2021-11-16 10:16:16 +0200

    import setting seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             LaratrustSeeder::class,
-            UsersTableSeeder::class
+            UsersTableSeeder::class,
+            SettingSeeder::class
         ]);
 
     }
