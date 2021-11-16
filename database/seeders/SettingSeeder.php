8cd1865a015d8df8e974a941af558e78598bc1bc
islamdiab-stack <islamdiab967@gmail.com>
2021-11-16 09:28:05 +0200

    create new seeder for setting and create new row

---
+++ database/seeders/SettingSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Setting;
+use Illuminate\Database\Seeder;
+
+class SettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Create new row for setting
+        Setting::create([
+            'web_name' => 'Web Name',
+            'phone_number' => +123456789,
+            'web_email' => 'web@app.com',
+            'address' => 'Egypt',
+        ]);
+    }
+}
