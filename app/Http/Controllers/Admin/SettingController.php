928281f22a2645c7ac00e23b813a5dffaa567c70
islamdiab-stack <islamdiab967@gmail.com>
2021-11-16 09:40:56 +0200

    create new controller for setting and type edit method for get data setting by id then return setting page

---
+++ app/Http/Controllers/Admin/SettingController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Setting;
+use Illuminate\Http\Request;
+
+class SettingController extends Controller
+{
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        // Get setting data by id
+        $setting = Setting::find($id);
+        return view('admin.setting.edit', compact('setting'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+}
