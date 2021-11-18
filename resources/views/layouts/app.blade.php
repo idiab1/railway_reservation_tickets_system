0615d293cb2590cde314390f9cf791d4c36c794e
islamdiab-stack <islamdiab967@gmail.com>
2021-11-18 12:10:41 +0200

    create new link for setting profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,6 +58,12 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+                                    <!-- Setting link -->
+                                    <a class="dropdown-item" href="{{route('profile.setting')}}">
+                                        {{ __('Setting') }}
+                                    </a>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
