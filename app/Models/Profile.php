481b5b28effda4d7fcd9e8c960f9a86bd16ea90f
islamdiab-stack <islamdiab967@gmail.com>
2021-11-18 11:27:05 +0200

    create new model for profile and create fillable array for add all columns then create new function for get arrtibute path image and create new relationship between profile, user where profile belong to user

---
+++ app/Models/Profile.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'image', 'facebook', 'twitter', 'linkedin', 'about', 'user_id'
+    ];
+
+    /***
+     * Get image path attribute
+     */
+    public function getImagePathAttr()
+    {
+        return asset('uploads/user/', $this->image);
+    }
+
+    /**
+     * Get the user that owns the Profile
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
