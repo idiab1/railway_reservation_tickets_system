755db4f7b66290fe8ef2a147878463ea978073d2
islamdiab-stack <islamdiab967@gmail.com>
2021-11-21 11:55:41 +0200

    create new page for contact home page

---
+++ resources/views/admin/contact/index.blade.php
@@ -0,0 +1,126 @@
+@extends('layouts.admin.app')
+
+{{-- Title --}}
+@section('title')
+    {{ trans('site.list_all_messages') }}
+@endsection
+
+{{-- Styles --}}
+@section('styles')
+<!-- DataTables -->
+<link rel="stylesheet" href="{{asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+<link rel="stylesheet" href="{{asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+<link rel="stylesheet" href="{{asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
+@endsection
+
+
+{{-- Page name --}}
+@section('page_name')
+    {{ trans('site.list_all_messages') }}
+@endsection
+
+{{-- Breadcrumb --}}
+@section('breadcrumb-item')
+    <li class="breadcrumb-item">{{ trans('site.messages') }}<li>
+@endsection
+
+{{-- Content --}}
+@section('content')
+    <section class="messages-section">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-12">
+                                <h3 class="card-title">{{ trans('site.list_all_messages') }}</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>{{ trans('site.name') }}</th>
+                                    <th>{{ trans('site.message') }}</th>
+                                    <th>{{ trans('site.action') }}</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @php
+                                    $id = 1;
+                                @endphp
+                                @if ($messages->count() > 0)
+                                    @foreach ($messages as $message)
+                                        <tr>
+                                            <td>{{$id++}}</td>
+                                            <td>{{$message->name}}</td>
+                                            <td>{{$message->message}}</td>
+                                            <td>
+                                                <a class="btn btn-primary btn-sm btn-edit d-inline-block" href="{{route('contact.show', ['id' => $message->id])}}">
+                                                    <i class="fas fa-eye"></i>
+                                                    {{ trans('site.show') }}
+                                                </a>
+                                                <form class="d-inline-block" action="{{route('contact.destroy', ['id' => $message->id])}}" method="POST">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button class="btn btn-danger btn-sm btn-delete" type="submit">
+                                                        <i class="fas fa-trash"></i>
+                                                        {{ trans('site.delete') }}
+                                                    </button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @endif
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th>#</th>
+                                    <th>{{ trans('site.name') }}</th>
+                                    <th>{{ trans('site.message') }}</th>
+                                    <th>{{ trans('site.action') }}</th>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                <!-- /.card-body -->
+                </div>
+            <!-- /.card -->
+            </div>
+            <!--/.col-12 -->
+        </div>
+        <!--/.row -->
+    </section>
+@endsection
+
+{{-- Scripts --}}
+@section('scripts')
+<!-- DataTables  & Plugins -->
+<script src="{{asset('plugins/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{asset('plugins/jszip/jszip.min.js')}}"></script>
+<script src="{{asset('plugins/pdfmake/pdfmake.min.js')}}"></script>
+<script src="{{asset('plugins/pdfmake/vfs_fonts.js')}}"></script>
+<script src="{{asset('plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
+
+<script>
+    $(function () {
+        $("#example1").DataTable({
+            "responsive": true,
+            "lengthChange": false,
+            "autoWidth": false,
+            "buttons": ["copy", "csv", "excel", "pdf", "print"]
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+
+    });
+</script>
+
+@endsection
