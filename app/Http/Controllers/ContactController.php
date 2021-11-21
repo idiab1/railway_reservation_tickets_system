9dc3b43dddbdbf54ebdd31586798e98e80cc3a9a
islamdiab-stack <islamdiab967@gmail.com>
2021-11-21 11:45:53 +0200

    type index method for return contact home page

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('contact.index');
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+}
