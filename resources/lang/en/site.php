e80969e0591537b9bf03e5a6c70469ff1d149a5d
islamdiab-stack <islamdiab967@gmail.com>
2021-12-12 15:05:43 +0200

    translate all keywords exist in messages page

--- resources/lang/en/site.php
+++ resources/lang/en/site.php
@@ -8,10 +8,19 @@ return [
     'messages' => 'Messages',
     'setting' => 'Setting',
 
+    // Messages Page
+    'message' => 'Message',
+    'list_all_messages' => 'list all messages',
+
+
 
 
     // Facades
-
+    'name' => 'Name',
+    'show' => 'Show',
+    'delete' => 'Delete',
+    'edit' => 'Edit',
+    'action' => 'Action',
 
     // Placeholder
     'search' => 'Search',
