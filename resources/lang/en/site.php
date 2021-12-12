630c51eb5f336769a81a93aa5817092baf2ebd61
islamdiab-stack <islamdiab967@gmail.com>
2021-12-12 20:19:13 +0200

    translate all keywords exist in contact page

--- resources/lang/en/site.php
+++ resources/lang/en/site.php
@@ -7,6 +7,7 @@ return [
     'users' => 'Users',
     'messages' => 'Messages',
     'setting' => 'Setting',
+    'contact_us' => 'Contact Us',
 
     // Messages Page
     'message' => 'Message',
@@ -17,17 +18,23 @@ return [
 
     // Facades
     'name' => 'Name',
-    'show' => 'Show',
-    'delete' => 'Delete',
-    'edit' => 'Edit',
     'action' => 'Action',
     'logout' => 'Logout',
     'login' => 'Login',
     'register' => 'Register',
+    'email' => 'email',
+
+    // Buttons
+    'send' => 'Send',
+    'show' => 'Show',
+    'delete' => 'Delete',
+    'edit' => 'Edit',
 
     // Placeholder
     'search' => 'Search',
-
+    'enter_name' => 'Enter Name',
+    'enter_email' => 'Enter your email address',
+    'enter_your_message' => 'Enter your message',
 
     // Footer
     'copyright' => 'Copyright All rights reserved 2021-2022.',
--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -2,7 +2,7 @@
 
 {{-- Title --}}
 @section('title')
-    {{ trans('site.contact') }}
+    {{ trans('site.contact_us') }}
 @endsection
 
 {{-- Page name --}}
@@ -13,48 +13,50 @@
 {{-- Content --}}
 @section('content')
 <div class="contact-page">
-    <div class="row">
-        <div class="col-md-6 m-auto">
-            <div class="setting-form">
-                <div class="card card-primary">
-                    <div class="card-header">
-                        <h3 class="h1 card-title">{{ trans('site.contact') }}</h3>
-                    </div>
-                    <!-- /.card-header -->
-                        <!-- form start -->
-                        <form action="{{route('contact.store')}}" method="POST">
-                            @csrf
-                            <div class="card-body">
-                                <!-- Name -->
-                                <div class="form-group">
-                                    <label for="name">{{ trans('site.name') }}</label>
-                                    <input class="form-control" type="text" id="name"
-                                    name="name" placeholder="{{trans('site.enter_name')}}">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7 m-auto">
+                <div class="setting-form">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="h1 card-title">{{ trans('site.contact_us') }}</h3>
+                        </div>
+                        <!-- /.card-header -->
+                            <!-- form start -->
+                            <form action="{{route('contact.store')}}" method="POST">
+                                @csrf
+                                <div class="card-body">
+                                    <!-- Name -->
+                                    <div class="form-group">
+                                        <label for="name">{{ trans('site.name') }}</label>
+                                        <input class="form-control" type="text" id="name"
+                                        name="name" placeholder="{{trans('site.enter_name')}}">
+                                    </div>
+
+                                    <!-- Email -->
+                                    <div class="form-group">
+                                        <label for="email">{{ trans('site.email') }}</label>
+                                        <input class="form-control" type="email" id="email"
+                                        name="email" placeholder="{{trans('site.enter_email')}}">
+                                    </div>
+
+                                    <!-- Contact -->
+                                    <div class="form-group">
+                                        <label for="message">{{ trans('site.message') }}</label>
+                                        <textarea class="form-control" name="message" id="message" cols="30"
+                                        rows="10" placeholder="{{trans('site.enter_your_message')}}"></textarea>
+                                    </div>
                                 </div>
+                                <!-- /.card-body -->
 
-                                <!-- Email -->
-                                <div class="form-group">
-                                    <label for="email">{{ trans('site.email') }}</label>
-                                    <input class="form-control" type="email" id="email"
-                                    name="email" placeholder="{{trans('site.enter_email')}}">
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary crayons-btn">{{ trans('site.send') }}</button>
                                 </div>
+                            </form>
 
-                                <!-- Contact -->
-                                <div class="form-group">
-                                    <label for="message">{{ trans('site.message') }}</label>
-                                    <textarea class="form-control" name="message" id="message" cols="30"
-                                    rows="10" placeholder="{{trans('site.enter_your_message')}}"></textarea>
-                                </div>
-                            </div>
-                            <!-- /.card-body -->
-
-                            <div class="card-footer">
-                                <button type="submit" class="btn btn-primary crayons-btn">{{ trans('site.send') }}</button>
-                            </div>
-                        </form>
-
-                    </div>
-            <!-- /.card -->
+                        </div>
+                <!-- /.card -->
+                </div>
             </div>
         </div>
     </div>
