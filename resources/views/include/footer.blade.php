4de4ecffc3548c1b28f9f954455cc2ef66469096
islamdiab-stack <islamdiab967@gmail.com>
2021-12-12 21:22:50 +0200

    create the footer component

---
+++ resources/views/include/footer.blade.php
@@ -0,0 +1,18 @@
+<!-- Footer -->
+<footer class="main-footer">
+
+    <!-- Copyright -->
+    <div class="copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="copyright-info text-center">
+                        <p>{{ trans('site.copyright') }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End of Copyright -->
+</footer>
+<!-- End Of Footer -->
