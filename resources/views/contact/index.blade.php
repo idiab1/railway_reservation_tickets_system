a2291ca3e7af65cb245717dc07daaba875c0b136
islamdiab-stack <islamdiab967@gmail.com>
2021-12-13 03:20:43 +0200

    add icons for every input

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -16,7 +16,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-7 m-auto">
-                <div class="setting-form">
+                <div class="contact-form">
                     <div class="card card-primary">
                         <div class="card-header">
                             <h3 class="h1 card-title">{{ trans('site.contact_us') }}</h3>
@@ -29,15 +29,30 @@
                                     <!-- Name -->
                                     <div class="form-group">
                                         <label for="name">{{ trans('site.name') }}</label>
-                                        <input class="form-control" type="text" id="name"
-                                        name="name" placeholder="{{trans('site.enter_name')}}">
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text" id="name">
+                                                    <i class="mdi mdi-account-outline"></i>
+                                                </span>
+                                            </div>
+                                            <input class="form-control" type="text" id="name" name="name"
+                                                placeholder="{{trans('site.name')}}" aria-describedby="name" aria-label="name" >
+                                        </div>
                                     </div>
 
                                     <!-- Email -->
                                     <div class="form-group">
                                         <label for="email">{{ trans('site.email') }}</label>
-                                        <input class="form-control" type="email" id="email"
-                                        name="email" placeholder="{{trans('site.enter_email')}}">
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text" id="email">
+                                                    <i class="mdi mdi-email"></i>
+                                                </span>
+                                            </div>
+                                            <input class="form-control" type="email" id="email" name="email"
+                                                placeholder="{{trans('site.email')}}" aria-describedby="email" aria-label="email" >
+                                        </div>
+
                                     </div>
 
                                     <!-- Contact -->
@@ -45,12 +60,16 @@
                                         <label for="message">{{ trans('site.message') }}</label>
                                         <textarea class="form-control" name="message" id="message" cols="30"
                                         rows="10" placeholder="{{trans('site.enter_your_message')}}"></textarea>
+
                                     </div>
                                 </div>
                                 <!-- /.card-body -->
 
-                                <div class="card-footer">
-                                    <button type="submit" class="btn btn-primary crayons-btn">{{ trans('site.send') }}</button>
+                                <div class="card-footer {{app()->getLocale() == "ar" ? "text-left" : "text-right" }}">
+                                    <button type="submit" class="btn btn-primary crayons-btn">
+                                        <i class="mdi mdi-send"></i>
+                                        {{ trans('site.send') }}
+                                    </button>
                                 </div>
                             </form>
 
