e7fdac7fd35c43e9931679a6a617804f3e6cb273
islamdiab-stack <islamdiab967@gmail.com>
2021-12-19 15:14:32 +0200

    create new column for return name of website

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -5,9 +5,16 @@
     <div class="copyright">
         <div class="container">
             <div class="row">
-                <div class="col-12">
+                <div class="col-md-6 col-sm-12">
+                    <!-- Barnd Logo -->
                     <div class="copyright-info text-center">
-                        <p>{{ trans('site.copyright') }}</p>
+                        <p>{{$setting->web_name}}</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <!-- Copyright -->
+                    <div class="copyright-info text-center">
+                        <p>&copy; {{ trans('site.copyright') }}</p>
                     </div>
                 </div>
             </div>
