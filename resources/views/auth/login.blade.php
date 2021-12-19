a74ebf56463c7018ee695d017f4ee9701cd32b63
islamdiab-stack <islamdiab967@gmail.com>
2021-12-19 16:39:56 +0200

    edit on layout for login page then add icons for every inputs exist in it

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
+{{-- Title --}}
+@section('title')
+    {{ trans('site.login') }}
+@endsection
+
 {{-- Header --}}
 @section('header')
 
@@ -7,73 +12,104 @@
 
 {{-- Main Content --}}
 @section('content')
-<div class="container">
+
+<!-- Login page -->
+<section class="login-page">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+            <!-- Sign Card -->
+            <div class="card sign-card">
+                <!-- Card Header -->
+                <div class="card-header">{{ trans('site.login') }}</div>
 
+                <!-- card body -->
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <!-- Email Address -->
+                        <div class="form-group">
+                            <label for="email">{{ trans('site.email') }}</label>
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <!-- Icon -->
+                                    <span class="input-group-text" id="email">
+                                        <i class="mdi mdi-email"></i>
+                                    </span>
+                                </div>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <!-- Input email field -->
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                    name="email" value="{{ old('email') }}" required autocomplete="email" autofocus
+                                    placeholder="{{trans('site.enter_email')}}" aria-describedby="email" aria-label="email" >
 
                                 @error('email')
+                                    <!-- If session has error type email -->
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
+
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <!-- Password -->
+                        <div class="form-group">
+                            <label for="password">{{ trans('site.password') }}</label>
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <!-- Icon -->
+                                    <span class="input-group-text" id="password">
+                                        <i class="mdi mdi-lock"></i>
+                                    </span>
+                                </div>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <!-- Input password field -->
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
+                                    name="password" required autocomplete="current-password"
+                                    placeholder="{{trans('site.enter_password')}}" aria-describedby="password" aria-label="password" >
 
                                 @error('password')
+                                    <!-- If session has error type password -->
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
+
                         </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <!-- Remember Me -->
+                        <div class="form-group">
+                            <div class="form-check">
+                                <!-- Check Input -->
+                                <input class="form-check-input" type="checkbox" name="remember"
+                                id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                                <label class="form-check-label" for="remember">
+                                    {{ trans('site.remember_me') }}
+                                </label>
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                        <!-- Button  -->
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary crayons-btn">
+                                {{ __('Login') }}
+                            </button>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
+                            @if (Route::has('password.request'))
+                                <!-- Forgot your password -->
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
+                            @endif
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</section>
+<!-- End of login page -->
 @endsection
