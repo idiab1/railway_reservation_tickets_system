f83e9652c333fdcfdadaa7d8e1ce92be3c53c3fd
islamdiab-stack <islamdiab967@gmail.com>
2022-01-17 17:39:56 +0200

    edit on the footer layout

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,23 +1,33 @@
 <!-- Footer -->
 <footer class="main-footer">
-
     <!-- Copyright -->
     <div class="copyright">
         <div class="container">
             <div class="row">
-                <div class="col-md-6 col-sm-12">
-                    <!-- Barnd Logo -->
-                    <div class="copyright-info text-center">
-                        <p>{{$setting->web_name}}</p>
-                    </div>
-                </div>
-                <div class="col-md-6 col-sm-12">
+                <div class="col-12">
                     <!-- Copyright -->
-                    <div class="copyright-info text-center">
-                        <p>&copy; {{ trans('site.copyright') }}</p>
+                    <div class="copyright">
+                        <div class="row">
+                            <div class="col-md-5 col-sm-12">
+                                <!-- Logo Brand -->
+                                <div class="logo-brand">
+                                    <h6 class="brand-name">{{$setting->web_name}}</h6>
+                                </div>
+                                <!-- ./end of logo Brand -->
+                            </div>
+                            <div class="col-md-2 col-sm-12 d-sm-none d-md-block">
+                                <i class="fas fa-arrow-circle-up"></i>
+                            </div>
+                            <div class="col-md-5 col-sm-12">
+                                <div class="copyright-info">
+                                    <p>&copy; 2022 {{ trans('site.copyright') }}</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                    <!-- ./end of copyright -->
                 </div>
-            </div>
+            </div> <!-- ./End of row -->
         </div>
     </div>
     <!-- End of Copyright -->
