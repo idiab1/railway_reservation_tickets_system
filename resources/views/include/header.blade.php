2fae0a2531fb5a37e628c3076cacb83e4f878cc2
islamdiab-stack <islamdiab967@gmail.com>
2022-01-17 19:04:08 +0200

    edit the header layout where create two columns for search on tickets

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,5 +1,41 @@
 <!-- Header -->
-<div class="main-header bg-dark" style="width: 100%; height: 350px;">
-
-</div>
+<header class="main-header bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 col-sm-12">
+                <div class="header-info text-light">
+                    <h1>Welcome to Egyption Railway</h1>
+                    <p>We saves your time both while burchasing, the check-in and during the travel </p>
+                </div>
+            </div>
+            <div class="col-md-7 col-sm-12">
+                <div class="form-reservation bg-light p-4">
+                    <!-- Form -->
+                    <form action="" method="POST">
+                        <!-- Travelling Routing -->
+                        <div class="form-group mb-3">
+                            <label for="travelling_routing" class="form-label">Travelling Routing</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" id="travelling_routing" placeholder="From" aria-label="From">
+                                <input type="text" class="form-control" id="travelling_routing" placeholder="To" aria-label="To">
+                            </div>
+                        </div>
+                        <!-- Travelling date -->
+                        <div class="form-group mb-3">
+                            <label for="travelling_routing" class="form-label">Travelling Date</label>
+                            <div class="input-group">
+                                <input type="date" class="form-control" id="travelling_routing" aria-label="From">
+                                <input type="date" class="form-control" id="travelling_routing" aria-label="To">
+                            </div>
+                        </div>
+                        <div class="text-right">
+                            <button type="submit" class="btn btn-primary">Search Tickets</button>
+                        </div>
+                    </form>
+                    <!-- ./end of form -->
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
 <!-- End of header -->
