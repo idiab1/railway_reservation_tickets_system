7f8eb8f78f2693da787569a3576f3d4f64a4b5d6
islamdiab-stack <islamdiab967@gmail.com>
2022-01-19 02:06:12 +0200

    create a new migration for the trains then create three columns for id, name, class_type

---
+++ database/migrations/2022_01_18_215627_create_trains_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 60);
+            $table->text('class_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
