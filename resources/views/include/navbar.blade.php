7da0befa42574beee865acd939f09d9512dd1299
islamdiab-stack <islamdiab967@gmail.com>
2022-01-19 03:18:08 +0200

    edit in class bg-white to bg-dark

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -1,5 +1,5 @@
 <!-- Navbar -->
-<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="{{route('home')}}">
             {{$setting->web_name}}
