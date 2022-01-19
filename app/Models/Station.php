2ae7090e0606560d711025c9d55ac3bf0e56be7d
islamdiab-stack <islamdiab967@gmail.com>
2022-01-19 02:09:27 +0200

    create a new model for the station then add columns to fillable array then create relation ship between station, and train. where station belongs to many of trains

---
+++ app/Models/Station.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Station extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ["name"];
+
+    /**
+     * The trains that belong to the Station
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function trains()
+    {
+        return $this->belongsToMany(Train::class, 'station_train');
+    }
+
+}
