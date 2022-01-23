74c499386b5a339d885aa26b376633aa69ad594f
islamdiab-stack <islamdiab967@gmail.com>
2022-01-23 12:29:13 +0200

    create new migration for types then create new columns for train_id, class_name, class price

---
+++ database/migrations/2022_01_23_062308_create_types_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('train_id')->unsigned();
+            $table->string('class_name', 60);
+            $table->double('class_price', 15, 8);
+            $table->timestamps();
+
+            $table->foreign('train_id')->references('id')->on('trains')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('types');
+    }
+}
