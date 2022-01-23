808ece98d9bc6ed7db805adfe2dd13588bdff0b4
islamdiab-stack <islamdiab967@gmail.com>
2022-01-23 12:31:05 +0200

    create a new model for type then add all columns in fillable and create a new relationship between types, train where all types belongs to train

---
+++ app/Models/Type.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Type extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'train_id', 'class_name', 'class_price'
+    ];
+
+    /**
+     * Get the train that owns the Type
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function train()
+    {
+        return $this->belongsTo(Train::class);
+    }
+
+}
