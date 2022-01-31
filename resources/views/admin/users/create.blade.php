08d1025d220b9ccae3f308b3d55d3d753b032333
islamdiab-stack <islamdiab967@gmail.com>
2022-01-31 21:44:30 +0200

    use trans method for translate add button

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -95,7 +95,7 @@
                             <!-- /.card-body -->
 
                             <div class="card-footer">
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <button type="submit" class="btn btn-primary btn-add btn-crayons">{{ trans('site.add') }}</button>
                             </div>
                         </form>
 
