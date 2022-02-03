5c7ee95dcba1be9a9b61c6a5c979a9ecc6b2b63a
islamdiab-stack <islamdiab967@gmail.com>
2022-02-04 01:48:48 +0200

    create a new form for add data of trains

---
+++ resources/views/admin/trains/create.blade.php
@@ -0,0 +1,137 @@
+@extends('layouts.admin.app')
+
+{{-- Title --}}
+@section('title')
+    {{ trans('site.add_train') }}
+@endsection
+
+{{-- Page name --}}
+@section('page_name')
+    {{ trans('site.add_train') }}
+@endsection
+
+{{-- Styles --}}
+@section('styles')
+
+    <!-- Select2 -->
+    <link rel="stylesheet" href="{{asset('plugins/select2/css/select2.min.css')}}">
+    <style>
+        .select2-container .select2-selection--single {
+            height: auto;
+        }
+
+    </style>
+@endsection
+
+{{-- Breadcrumb --}}
+@section('breadcrumb-item')
+    <li class="breadcrumb-item"><a href="{{route('trains.index')}}">{{ trans('site.trains') }}</a></li>
+    <li class="breadcrumb-item">{{ trans('site.add_train') }}<li>
+@endsection
+
+{{-- Content --}}
+@section('content')
+<div class="trains-page">
+    <div class="row">
+        <div class="col-md-8 m-auto">
+            <div class="trains-form">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">{{ trans('site.add_train') }}</h3>
+                    </div>
+                    <!-- /.card-header -->
+                        <!-- form start -->
+                        <form action="{{route('trains.store')}}" method="POST">
+                            @csrf
+                            <div class="card-body">
+                                <!-- Name -->
+                                <div class="form-group">
+                                    <label for="name">{{ trans('site.name') }}</label>
+                                    <input type="text" class="form-control" id="name"
+                                            name="name" placeholder="{{trans('site.enter_name_train')}}">
+                                </div>
+
+                                <!-- Depature Station / Time -->
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col">
+                                            <label for="depature_station">{{ trans('site.depature_station') }}</label>
+                                            <!-- All stations -->
+                                            <select class="form-control select2 searchable" name="depature_station_id" id="depature_station">
+                                                <option value="" >{{trans('site.all_stations')}}</option>
+                                                @foreach ($stations as $station)
+                                                    <option value="{{$station->id}}" >{{$station->name}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="col">
+                                            <label for="name">{{ trans('site.depature_time') }}</label>
+                                            <input type="datetime-local" class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- Arrival Station / Time -->
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col">
+                                            <label for="arrival_station">{{ trans('site.arrival_station') }}</label>
+                                            <!-- All stations -->
+                                            <select class="form-control select2 searchable" name="arrival_station_id" id="arrival_station">
+                                                <option value="" >{{trans('site.all_stations')}}</option>
+                                                @foreach ($stations as $station)
+                                                    <option value="{{$station->id}}" >{{$station->name}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="col">
+                                            <label for="name">{{ trans('site.arrival_time') }}</label>
+                                            <input type="datetime-local" class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                            <!-- /.card-body -->
+
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary btn-add btn-crayons">
+                                    {{ trans('site.add') }}
+                                </button>
+                            </div>
+                        </form>
+
+                    </div>
+            <!-- /.card -->
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('scripts')
+<!-- Select 2 -->
+<script src="{{asset('plugins/select2/js/select2.min.js')}}"></script>
+<script>
+    $(document).ready(function(){
+        $('.select2').select2();
+        // Image Preview
+        $('.image').change(function(){
+            if(this.files && this.files[0]){
+
+                let reader = new FileReader();
+
+                reader.onload = function(e){
+
+                    $('.preview').attr('src', e.target.result);
+
+                }
+                reader.readAsDataURL(this.files[0]);
+
+            }
+        })
+
+    });
+</script>
+@endsection
