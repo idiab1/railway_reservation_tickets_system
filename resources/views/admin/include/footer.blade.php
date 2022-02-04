f5b2ba6c9786a5951bd8f1f8f26b69012b6d27ab
islamdiab-stack <islamdiab967@gmail.com>
2022-02-04 02:11:52 +0200

    edit on the layout footer component

--- resources/views/admin/include/footer.blade.php
+++ resources/views/admin/include/footer.blade.php
@@ -1,7 +1,10 @@
 <!-- Footer -->
 <footer class="main-footer">
-    <strong>&copy; {{ trans('site.copyright') }}
-        <a href="{{route('admin.home')}}">{{$setting->web_name}}</a>.
-    </strong>
+    <p class="m-0">
+        &copy; {{ trans('site.copyright') }}
+        <strong>
+            <a href="{{route('admin.home')}}">{{$setting->web_name}}</a>.
+        </strong>
+    </p>
 </footer>
 <!-- End of footer -->
