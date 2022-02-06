040ef6ca6e0ff02c51642c677c7e00c205a74dab
islamdiab-stack <islamdiab967@gmail.com>
2022-02-06 19:24:30 +0200

    create a new model for post then add all name of columns in fillable array then create the relationship between posts, users where all posts belongs to user

---
+++ app/Models/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'slug', 'content', 'image', 'user_id'
+    ];
+
+    /**
+     * Get the user that owns the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
