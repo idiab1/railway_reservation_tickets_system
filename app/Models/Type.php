78b5c52c6e5dc32f4463a92b62ffebb9ccaeab1e
islamdiab-stack <islamdiab967@gmail.com>
2022-02-23 16:51:44 +0200

    add name to fillable array

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -9,18 +9,16 @@ class Type extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'train_id', 'class_name', 'class_price', 'seats_count'
-    ];
+    protected $fillable = ['name'];
 
-    /**
-     * Get the train that owns the Type
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function train()
-    {
-        return $this->belongsTo(Train::class);
-    }
+    // /**
+    //  * Get the train that owns the Type
+    //  *
+    //  * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+    //  */
+    // public function train()
+    // {
+    //     return $this->belongsTo(Train::class);
+    // }
 
 }
