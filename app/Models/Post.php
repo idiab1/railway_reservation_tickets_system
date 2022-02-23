7a85643c5e2332b7c2b289b7998c45e8b2520349
islamdiab-stack <islamdiab967@gmail.com>
2022-02-23 16:48:56 +0200

    use soft deteles and add date of delete in dates array

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,14 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use HasFactory;
 
+    use SoftDeletes;
+
     protected $fillable = [
         'name', 'slug', 'content', 'image', 'user_id'
     ];
+
+    protected $dates = ['deleted_at'];
 
     /**
      * Get the user that owns the Post
