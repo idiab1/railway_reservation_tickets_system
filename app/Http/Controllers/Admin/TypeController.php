e672913c1f8444cd76694dfb4ebac4935236e592
islamdiab-stack <islamdiab967@gmail.com>
2022-02-24 17:56:04 +0200

    write index method for return all data of types and send to types homepage

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class TypeController extends Controller
@@ -14,7 +15,9 @@ class TypeController extends Controller
      */
     public function index()
     {
-        //
+        // Get all data of types
+        $types = Type::all();
+        return view('admin.types.index', compact('types'));
     }
 
     /**
