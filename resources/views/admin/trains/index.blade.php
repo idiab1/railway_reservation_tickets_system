117836c23e68863ca536ffef0643c1eeff99cb2b
islamdiab-stack <islamdiab967@gmail.com>
2022-02-24 22:51:51 +0200

    add a new class to table element

--- resources/views/admin/trains/index.blade.php
+++ resources/views/admin/trains/index.blade.php
@@ -52,7 +52,7 @@
 
                     <!-- Card body -->
                     <div class="card-body">
-                        <table id="example1" class="table table-bordered table-striped">
+                        <table id="example1" class="table table-responsive table-bordered table-striped">
                             <thead>
                                 <tr>
                                     <th>#</th>
