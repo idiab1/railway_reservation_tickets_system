1837243b0320eed8d91f58482d202a09f3d2e472
islamdiab-stack <islamdiab967@gmail.com>
2022-02-26 07:48:47 +0200

    add depature_station, arrival_station to train seeder

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -14,17 +14,18 @@ class TrainSeeder extends Seeder
      */
     public function run()
     {
-        Train::create([
+        $train = Train::create([
             'name' => 'train 25634',
             'depature_at' => '2022-02-07 01:24:46',
             'arrival_at' => '2022-02-07 04:30:00',
-            'depature_station' => 'Mansoura Station',
-            'arrival_station' => 'Cairo Station',
+            'depature_station' => 'Cairo Station',
+            'arrival_station' => 'Alexandria Station',
             'status' => 0,
-            'station_id' => 1,
             'seats_count' => 100,
             'train_type' => "General class",
         ]);
+
+        $train->stations()->attach([1, 2]);
 
     }
 }
