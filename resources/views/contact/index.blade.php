8a529ad115e334493a57eda935ad633300db6bea
islamdiab-stack <islamdiab967@gmail.com>
2022-02-28 18:08:05 +0200

    add required attribute

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -77,7 +77,8 @@
                                                             </span>
                                                         </div>
                                                         <input class="form-control" type="text" id="name" name="name"
-                                                            placeholder="{{trans('site.name')}}" aria-describedby="name" aria-label="name" >
+                                                            placeholder="{{trans('site.name')}}" aria-describedby="name"
+                                                            aria-label="name" required>
                                                     </div>
                                                 </div>
                                                 <!-- End of Name -->
@@ -93,22 +94,20 @@
                                                             </span>
                                                         </div>
                                                         <input class="form-control" type="email" id="email" name="email"
-                                                            placeholder="{{trans('site.email')}}" aria-describedby="email" aria-label="email" >
+                                                            placeholder="{{trans('site.email')}}" aria-describedby="email"
+                                                            aria-label="email" required>
                                                     </div>
                                                 </div>
                                                 <!-- End of Email address -->
                                             </div>
                                         </div>
 
-
-
-
-
                                         <!-- Message -->
-                                        <div class="form-group">
+                                        <div class="form-group m-0">
                                             <label for="message">{{ trans('site.message') }}</label>
                                             <textarea class="form-control" name="message" id="message" cols="30"
-                                            rows="10" placeholder="{{trans('site.enter_your_message')}}"></textarea>
+                                                rows="10" placeholder="{{trans('site.enter_your_message')}}"
+                                                required autofocus></textarea>
 
                                         </div>
                                     </div>
@@ -187,78 +186,9 @@
                 </div>
             </div>
         </div>
-
-
-        {{-- <div class="row">
-            <div class="col-md-7 m-auto">
-                <!-- Section form -->
-                <div class="contact-form section-form">
-                    <div class="card card-primary">
-                        <div class="card-header">
-                            <h3 class="h1 card-title">{{ trans('site.contact_us') }}</h3>
-                        </div>
-                        <!-- /.card-header -->
-                        <!-- form -->
-                        <form action="{{route('contact.store')}}" method="POST">
-                            @csrf
-                            <!-- Card body -->
-                            <div class="card-body">
-                                <!-- Name -->
-                                <div class="form-group">
-                                    <label for="name">{{ trans('site.name') }}</label>
-                                    <div class="input-group mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" id="name">
-                                                <i class="mdi mdi-account-outline"></i>
-                                            </span>
-                                        </div>
-                                        <input class="form-control" type="text" id="name" name="name"
-                                            placeholder="{{trans('site.name')}}" aria-describedby="name" aria-label="name" >
-                                    </div>
-                                </div>
-
-                                <!-- Email -->
-                                <div class="form-group">
-                                    <label for="email">{{ trans('site.email') }}</label>
-                                    <div class="input-group mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" id="email">
-                                                <i class="mdi mdi-email"></i>
-                                            </span>
-                                        </div>
-                                        <input class="form-control" type="email" id="email" name="email"
-                                            placeholder="{{trans('site.email')}}" aria-describedby="email" aria-label="email" >
-                                    </div>
-
-                                </div>
-
-                                <!-- Contact -->
-                                <div class="form-group">
-                                    <label for="message">{{ trans('site.message') }}</label>
-                                    <textarea class="form-control" name="message" id="message" cols="30"
-                                    rows="10" placeholder="{{trans('site.enter_your_message')}}"></textarea>
-
-                                </div>
-                            </div>
-                            <!-- End of card body -->
-
-                            <!-- Card footer -->
-                            <div class="card-footer {{app()->getLocale() == "ar" ? "text-left" : "text-right" }}">
-                                <button type="submit" class="btn btn-primary crayons-btn">
-                                    <i class="mdi mdi-send"></i>
-                                    {{ trans('site.send') }}
-                                </button>
-                            </div>
-                            <!-- End fo card footer -->
-                        </form>
-                        <!-- End of form -->
-                    </div>
-                    <!-- Ened of card -->
-                </div>
-                <!-- End fo section form -->
-            </div>
-        </div> --}}
     </div>
     <!-- End of container fluid -->
 </section>
 @endsection
+
+
