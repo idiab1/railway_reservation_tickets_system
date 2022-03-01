aada8f1aa03213e7e3a85d1b59d2e9c1248264e7
islamdiab-stack <islamdiab967@gmail.com>
2022-03-01 13:54:41 +0200

    change on the layout of header

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,38 +1,59 @@
 <!-- Header -->
-<header class="main-header bg-dark">
-    <div class="container">
+<header class="main-header bg-dark" style="height:500px">
+    <div class="container-fluid">
+        <!-- Header info -->
         <div class="row">
-            <div class="col-md-5 col-sm-12">
-                <div class="header-info text-light">
+            <div class="col-8">
+                <div class="header-info text-light tex-center">
                     <h1>Welcome to Egyption Railway</h1>
                     <p>We saves your time both while burchasing, the check-in and during the travel </p>
                 </div>
             </div>
-            <div class="col-md-7 col-sm-12">
-                <div class="form-reservation bg-light p-4">
-                    <!-- Form -->
-                    <form action="" method="POST">
-                        <!-- Travelling Routing -->
-                        <div class="form-group mb-3">
-                            <label for="travelling_routing" class="form-label">Travelling Routing</label>
-                            <div class="input-group">
-                                <input type="text" class="form-control" id="travelling_routing" placeholder="From" aria-label="From">
-                                <input type="text" class="form-control" id="travelling_routing" placeholder="To" aria-label="To">
+        </div>
+        <!-- End of Header info -->
+
+        <div class="row">
+            <div class="col-12">
+                <div class="reservation-box bg-light p-4">
+                    <!-- Reservation form -->
+                    <div class="reservation-form">
+                        <!-- Form -->
+                        <form action="" method="POST">
+                            <div class="row">
+                                <div class="col">
+                                    <!-- Travelling Routing -->
+                                    <div class="form-group mb-3">
+                                        <label for="travelling_routing" class="form-label">
+                                            Travelling Routing
+                                        </label>
+                                        <div class="input-group">
+                                            <input type="text" class="form-control" id="travelling_routing" placeholder="From" aria-label="From">
+                                            <input type="text" class="form-control" id="travelling_routing" placeholder="To" aria-label="To">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <!-- Travelling date -->
+                                    <div class="form-group mb-3">
+                                        <label for="travelling_routing" class="form-label">
+                                            Travelling Date
+                                        </label>
+                                        <div class="input-group">
+                                            <input type="date" class="form-control" id="travelling_routing" aria-label="From">
+                                            <input type="date" class="form-control" id="travelling_routing" aria-label="To">
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <!-- Travelling date -->
-                        <div class="form-group mb-3">
-                            <label for="travelling_routing" class="form-label">Travelling Date</label>
-                            <div class="input-group">
-                                <input type="date" class="form-control" id="travelling_routing" aria-label="From">
-                                <input type="date" class="form-control" id="travelling_routing" aria-label="To">
+
+
+                            <div class="text-right">
+                                <button type="submit" class="btn btn-primary">Search Tickets</button>
                             </div>
-                        </div>
-                        <div class="text-right">
-                            <button type="submit" class="btn btn-primary">Search Tickets</button>
-                        </div>
-                    </form>
-                    <!-- ./end of form -->
+                        </form>
+                        <!-- ./end of form -->
+                    </div>
+                    <!-- End fo reservation form -->
                 </div>
             </div>
         </div>
