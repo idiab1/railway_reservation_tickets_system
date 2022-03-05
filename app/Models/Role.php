deccac0f865d41fca8ef9bb9507eba8f8b37038f
islamdiab-stack <islamdiab967@gmail.com>
2022-03-05 12:44:40 +0200

    create a relation ship between users, roles where roles belongs to many of users

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,4 +7,16 @@ use Laratrust\Models\LaratrustRole;
 class Role extends LaratrustRole
 {
     public $guarded = [];
+
+
+    /**
+     * The users that belong to the Role
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'role_user');
+    }
+
 }
