9b78e133c1bcfd10c93ef654335d599064a95f8e
islamdiab-stack <islamdiab967@gmail.com>
2022-03-05 15:26:48 +0200

    add new links for add new button, and remove form element

--- resources/views/admin/include/navbar.blade.php
+++ resources/views/admin/include/navbar.blade.php
@@ -1,20 +1,21 @@
 <!-- Navbar -->
-<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+<nav class="main-header navbar navbar-expand ">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
         <li class="nav-item">
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
+        <!-- Overview -->
         <li class="nav-item d-none d-sm-inline-block">
-            <a class="nav-link" href="{{route('home')}}" target="_blank">{{ trans('site.home') }}</a>
+            <a class="nav-link" href="{{route('home')}}" target="_blank">{{ trans('site.overview') }}</a>
         </li>
-        <li class="nav-item d-none d-sm-inline-block">
+        {{-- <li class="nav-item d-none d-sm-inline-block">
             <a class="nav-link" href="{{route('setting.edit', ['id' => $setting->id])}}">{{ trans('site.system_settings') }}</a>
-        </li>
+        </li> --}}
     </ul>
 
     <!-- SEARCH FORM -->
-    <form class="form-inline ml-3 mr-3">
+    {{-- <form class="form-inline ml-3 mr-3">
         <div class="input-group input-group-sm">
             <input class="form-control form-control-navbar" type="search" placeholder="{{ trans('site.search') }}" aria-label="Search">
             <div class="input-group-append">
@@ -23,13 +24,13 @@
                 </button>
             </div>
         </div>
-    </form>
+    </form> --}}
 
     <!-- Right navbar links -->
     <ul class="navbar-nav {{app()->getLocale() == 'ar' ? "mr-auto" : "ml-auto"}}">
 
         <!-- Notifications Dropdown Menu -->
-        <li class="nav-item dropdown">
+        {{-- <li class="nav-item dropdown">
             <a class="nav-link" data-toggle="dropdown" href="#">
             <i class="far fa-bell"></i>
             <span class="badge badge-warning navbar-badge">15</span>
@@ -54,11 +55,11 @@
             <div class="dropdown-divider"></div>
             <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
             </div>
-        </li>
+        </li> --}}
 
         <!-- Languages -->
         <li class="nav-item nav-language dropdown d-none d-md-block">
-            <a class="nav-link dropdown-toggle" id="languageDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link pr-0 dropdown-toggle" id="languageDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
                 <div class="nav-language-icon d-inline-block">
                     @if (app()->getLocale() == 'en')
                         <i class="flag-icon flag-icon-us" title="us" id="us"></i>
@@ -74,21 +75,21 @@
             </a>
             <div class="dropdown-menu navbar-dropdown" aria-labelledby="languageDropdown">
                 @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
-                    <a class="dropdown-item" rel="alternate" hreflang="{{ $localeCode }}"
+                    <a class="dropdown-item " rel="alternate" hreflang="{{ $localeCode }}"
                         href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
                         <div class="nav-language-text">
                             <p class="mb-1 text-black">{{ $properties['native'] }}</p>
                         </div>
                     </a>
-                    <div class="dropdown-divider"></div>
                 @endforeach
             </div>
         </li>
 
-        <li class="nav-item dropdown">
+        <!-- User dropdown -->
+        <li class="nav-item dropdown user-dropdown">
             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                 <div class="image d-inline-block mr-2 ml-2">
-                    <img class="img-circle border border-dark elevation-2" width="20px" src="{{asset("uploads/users/". Auth::user()->profile->image)}}" alt="User Image">
+                    <img class="img-circle border border-dark elevation-2" width="30px" src="{{asset("uploads/users/". Auth::user()->profile->image)}}" alt="User Image">
                 </div>
                 {{ Auth::user()->name }}
             </a>
@@ -114,7 +115,33 @@
                 </form>
             </div>
         </li>
+        <!-- End of user dropdown -->
 
+        <!-- Add new button -->
+        <li class="nav-item dropdown add-new">
+            <a class="nav-link btn btn-secondaryy btn-add-new dropdown-toggle"
+                id="navbarDropdown" href="#" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false" v-pre>
+                <!-- Icon -->
+                <i class="fas fa-plus"></i>
+                {{-- {{ trans('site.add_new') }} --}}
+            </a>
+
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <!-- Dropdown menu links -->
+                <a class="dropdown-item" href="{{route('posts.create')}}">
+                    {{ trans('site.post') }}
+                </a>
+                <a class="dropdown-item" href="{{route('stations.create')}}">
+                    {{ trans('site.station') }}
+                </a>
+                <a class="dropdown-item" href="{{route('trains.create')}}">
+                    {{ trans('site.ticket') }}
+                </a>
+
+            </div>
+        </li>
+        <!-- End of add new button -->
 
     </ul>
 </nav>
