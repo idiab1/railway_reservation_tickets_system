e6d23527a0d5940785eed8bfe9304734332e8d5a
islamdiab-stack <islamdiab967@gmail.com>
2022-03-07 14:47:48 +0200

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,8 @@
 
 // Get height of navbar
 let navbarH = document.querySelector(".navbar").clientHeight;
-// Get height of footer
-let footerH = document.querySelector(".main-footer").clientHeight;
 
 // Adjust calculate marginTop for body html
 document.querySelector("body").style.marginTop = navbarH + "px";
 
-
-// Adjust calculate minimum height for section
-// Select on section element
-const section = document.querySelector(".section");
-section.style.minHeight = ((window.innerHeight) - ( footerH +  navbarH)) + "px";
 
