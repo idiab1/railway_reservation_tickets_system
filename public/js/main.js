ab91a5ca0997a95326eea8973cae1e79bf9b6e56
islamdiab-stack <islamdiab967@gmail.com>
2022-03-07 12:43:59 +0200

    adjust calculate margin top for body

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,15 @@
-// Adjust calculate minimum height
 
-// Select on section element
-const section = document.querySelector(".section");
 // Get height of navbar
 let navbarH = document.querySelector(".navbar").clientHeight;
 // Get height of footer
 let footerH = document.querySelector(".main-footer").clientHeight;
 
+// Adjust calculate marginTop for body html
+document.querySelector("body").style.marginTop = navbarH + "px";
+
+
+// Adjust calculate minimum height for section
+// Select on section element
+const section = document.querySelector(".section");
 section.style.minHeight = ((window.innerHeight) - ( footerH +  navbarH)) + "px";
 
