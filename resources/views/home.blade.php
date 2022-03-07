8704d8560f0f6e86773fcfc3416a50b4690cd868
islamdiab-stack <islamdiab967@gmail.com>
2022-03-07 12:13:55 +0200

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,96 @@
 @endsection
 
 @section('content')
+    <!-- Ticket Advantages -->
+    <section class="e-ticket-advantages">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <!-- Header of section -->
+                    <h2 class="heading">E-Tickets Advantages</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <!-- Advantage card -->
+                    <div class="card advantage-card text-center">
+                        <!-- Advantage Header -->
+                        <div class="card-header advantage-header">
+                            <!-- Advantage icon -->
+                            <div class="advantage-icon">
+                                <i class="fas fa-ticket-alt fa-2x"></i>
+                            </div>
+                            <!-- Name of advantage -->
+                            <h2></h2>
+                        </div>
+                        <!-- Advantage inforamtion -->
+                        <div class="card-body advantage-info">
+                            <p>It's very easy to order a train ticket online. E-Ticket can not be
+                                lost of forgotten at home.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <!-- Advantage card -->
+                    <div class="card advantage-card text-center">
+                        <!-- Advantage Header -->
+                        <div class="card-header advantage-header">
+                            <!-- Advantage icon -->
+                            <div class="advantage-icon">
+                                <i class="fas fa-building fa-2x"></i>
+                            </div>
+                            <!-- Name of advantage -->
+                            <h2></h2>
+                        </div>
+                        <!-- Advantage inforamtion -->
+                        <div class="card-body advantage-info">
+                            <p>There's no need to go to the ticket office additionally before your
+                                trip. Save your money and nerves.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <!-- Advantage card -->
+                    <div class="card advantage-card text-center">
+                        <!-- Advantage Header -->
+                        <div class="card-header advantage-header">
+                            <!-- Advantage icon -->
+                            <div class="advantage-icon">
+                                <i class="fas fa-globe fa-2x"></i>
+                            </div>
+                            <!-- Name of advantage -->
+                            <h2></h2>
+                        </div>
+                        <!-- Advantage inforamtion -->
+                        <div class="card-body advantage-info">
+                            <p>To order ticekts all you need is internet, a couple minutes and
+                                a payment card. Grab bonuses and discounts.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <!-- Advantage card -->
+                    <div class="card advantage-card text-center">
+                        <!-- Advantage Header -->
+                        <div class="card-header advantage-header">
+                            <!-- Advantage icon -->
+                            <div class="advantage-icon">
+                                <i class="fas fa-credit-card fa-2x"></i>
+                            </div>
+                            <!-- Name of advantage -->
+                            <h2></h2>
+                        </div>
+                        <!-- Advantage inforamtion -->
+                        <div class="card-body advantage-info">
+                            <p>Pay through any payment mode & avail instant ticket refund and cancellation</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End of Ticket Advantages -->
+
     <!-- Recent Posts -->
     <section class="recent-posts">
         <div class="container">
@@ -129,95 +219,6 @@
             </div>
         </div>
     </section>
-    <!-- Ticket Advantages -->
-    <section class="e-ticket-advantages">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <!-- Header of section -->
-                    <h2 class="heading">E-Tickets Advantages</h2>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-3">
-                    <!-- Advantage card -->
-                    <div class="card advantage-card text-center">
-                        <!-- Advantage Header -->
-                        <div class="card-header advantage-header">
-                            <!-- Advantage icon -->
-                            <div class="advantage-icon">
-                                <i class="fas fa-ticket-alt fa-2x"></i>
-                            </div>
-                            <!-- Name of advantage -->
-                            <h2></h2>
-                        </div>
-                        <!-- Advantage inforamtion -->
-                        <div class="card-body advantage-info">
-                            <p>It's very easy to order a train ticket online. E-Ticket can not be
-                                lost of forgotten at home.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <!-- Advantage card -->
-                    <div class="card advantage-card text-center">
-                        <!-- Advantage Header -->
-                        <div class="card-header advantage-header">
-                            <!-- Advantage icon -->
-                            <div class="advantage-icon">
-                                <i class="fas fa-building fa-2x"></i>
-                            </div>
-                            <!-- Name of advantage -->
-                            <h2></h2>
-                        </div>
-                        <!-- Advantage inforamtion -->
-                        <div class="card-body advantage-info">
-                            <p>There's no need to go to the ticket office additionally before your
-                                trip. Save your money and nerves.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <!-- Advantage card -->
-                    <div class="card advantage-card text-center">
-                        <!-- Advantage Header -->
-                        <div class="card-header advantage-header">
-                            <!-- Advantage icon -->
-                            <div class="advantage-icon">
-                                <i class="fas fa-globe fa-2x"></i>
-                            </div>
-                            <!-- Name of advantage -->
-                            <h2></h2>
-                        </div>
-                        <!-- Advantage inforamtion -->
-                        <div class="card-body advantage-info">
-                            <p>To order ticekts all you need is internet, a couple minutes and
-                                a payment card. Grab bonuses and discounts.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <!-- Advantage card -->
-                    <div class="card advantage-card text-center">
-                        <!-- Advantage Header -->
-                        <div class="card-header advantage-header">
-                            <!-- Advantage icon -->
-                            <div class="advantage-icon">
-                                <i class="fas fa-credit-card fa-2x"></i>
-                            </div>
-                            <!-- Name of advantage -->
-                            <h2></h2>
-                        </div>
-                        <!-- Advantage inforamtion -->
-                        <div class="card-body advantage-info">
-                            <p>Pay through any payment mode & avail instant ticket refund and cancellation</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Ticket Advantages -->
-
+    <!-- End of recent posts -->
 
 @endsection
