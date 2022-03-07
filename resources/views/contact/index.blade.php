ab95b790717f195ffe1afa7b1fc3410027ce7af2
islamdiab-stack <islamdiab967@gmail.com>
2022-03-07 13:33:33 +0200

    update

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -203,34 +203,30 @@
 @section('footer')
 <!-- Footer -->
 <footer class="main-footer">
-    <!-- Copyright -->
-    <div class="copyright">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <!-- Copyright -->
-                    <div class="copyright">
-                        <div class="row">
-                            <div class="col-md-6 col-sm-12">
-                                <!-- Logo Brand -->
-                                <div class="logo-brand">
-                                    <h6 class="brand-name">{{$setting->web_name}}</h6>
-                                </div>
-                                <!-- ./end of logo Brand -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <!-- Copyright -->
+                <div class="copyright">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12">
+                            <!-- Logo Brand -->
+                            <div class="logo-brand">
+                                <h6 class="brand-name">{{$setting->web_name}}</h6>
                             </div>
-                            <div class="col-md-6 col-sm-12">
-                                <div class="copyright-info text-right">
-                                    <p>&copy; 2022 {{ trans('site.copyright') }}</p>
-                                </div>
+                            <!-- ./end of logo Brand -->
+                        </div>
+                        <div class="col-md-6 col-sm-12">
+                            <div class="copyright-info text-right">
+                                <p>&copy; 2022 {{ trans('site.copyright') }}</p>
                             </div>
                         </div>
                     </div>
-                    <!-- ./end of copyright -->
                 </div>
-            </div> <!-- ./End of row -->
-        </div>
+                <!-- ./end of copyright -->
+            </div>
+        </div> <!-- ./End of row -->
     </div>
-    <!-- End of Copyright -->
 </footer>
 <!-- End Of Footer -->
 @endsection
