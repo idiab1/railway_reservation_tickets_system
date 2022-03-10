30be19c28d5ce3da0bcad46cb4b329d04f8bcf10
islamdiab-stack <islamdiab967@gmail.com>
2022-03-11 01:47:59 +0200

    change the layout for edit station page

--- resources/views/admin/stations/edit.blade.php
+++ resources/views/admin/stations/edit.blade.php
@@ -18,39 +18,66 @@
 
 {{-- Content --}}
 @section('content')
-<div class="stations-page">
+<section class="stations-section section">
     <div class="row">
         <div class="col-md-8 m-auto">
-            <div class="stations-form">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">{{ trans('site.edit') }} {{$station->name . "'s"}}</h3>
-                    </div>
-                    <!-- /.card-header -->
-                        <!-- form start -->
-                        <form action="{{route('stations.update', ['id' => $station->id])}}" method="POST"  enctype="multipart/form-data">
-                            @csrf
-                            @method('PUT')
-                            <div class="card-body">
-                                <!-- Name -->
-                                <div class="form-group">
-                                    <label for="name">{{ trans('site.name') }}</label>
-                                    <input class="form-control" type="text" id="name" name="name"
-                                    value="{{$station->name}}" placeholder="{{trans('site.admin_enter_name')}}">
+
+            <!-- form container -->
+            <div class="form-container">
+                <div class="row">
+                    <div class="col-md-7 p-0">
+                        <!-- Stations form -->
+                        <div class="stations-form section-form">
+                            <!-- Card -->
+                            <div class="card">
+                                <!-- Card header -->
+                                <div class="card-header">
+                                    <h3 class="card-title">
+                                        {{ trans('site.edit') }} {{$station->name . "'s"}}
+                                    </h3>
                                 </div>
+                                <!-- End of card header -->
+                                <!-- form -->
+                                <form action="{{route('stations.update', ['id' => $station->id])}}" method="POST"  enctype="multipart/form-data">
+                                    @csrf
+                                    @method('PUT')
+                                    <div class="card-body">
+                                        <!-- Name -->
+                                        <div class="form-group m-0">
+                                            <label for="name">{{ trans('site.name') }}</label>
+                                            <input class="form-control" type="text" id="name" name="name"
+                                            value="{{$station->name}}" placeholder="{{trans('site.admin_enter_name')}}">
+                                        </div>
+
+                                    </div>
+                                    <!-- /.card-body -->
+
+                                    <div class="card-footer">
+                                        <button type="submit" class="btn btn-primary btn-add btn-crayons">
+                                            {{ trans('site.update') }}
+                                        </button>
+                                    </div>
+                                </form>
+                                <!-- End of form -->
 
                             </div>
-                            <!-- /.card-body -->
-
-                            <div class="card-footer">
-                                <button type="submit" class="btn btn-primary btn-crayons btn-update">{{ trans('site.update') }}</button>
-                            </div>
-                        </form>
-
+                            <!-- End of card -->
+                        </div>
+                        <!-- End of Stations form -->
                     </div>
-            <!-- /.card -->
+                    <div class="col-md-5 p-0">
+                        <!-- Stations Info -->
+                        <div class="station-info section-info">
+                            <div class="card">
+                                <div class="card-body"></div>
+                            </div>
+                        </div>
+                        <!-- End of Stations Info -->
+                    </div>
+                </div>
             </div>
+            <!-- End of form container -->
         </div>
     </div>
-</div>
+</section>
 @endsection
