1122b9f05a5fb8abcc6a8506f6e3d2d465305673
islamdiab-stack <islamdiab967@gmail.com>
2022-03-10 14:53:40 +0200

    change the layout trains home page

--- resources/views/admin/trains/index.blade.php
+++ resources/views/admin/trains/index.blade.php
@@ -17,6 +17,10 @@
 {{-- Page name --}}
 @section('page_name')
     {{ trans('site.list_trains') }}
+    <button type="button" class="btn btn-create btn-sm btn-primary btn-crayons"
+        data-toggle="modal" data-target="#createNewItem">
+        <i class="fas fa-plus"></i>
+    </button>
 @endsection
 
 {{-- Breadcrumb --}}
@@ -30,7 +34,7 @@
         <div class="row">
             <div class="col-12">
                 <div class="card">
-                    <!-- Card Header -->
+                    {{-- <!-- Card Header -->
                     <div class="card-header">
                         <div class="row">
                             <div class="col-6">
@@ -48,11 +52,11 @@
                             </div>
                         </div>
                     </div>
-                    <!-- /End of card-header -->
+                    <!-- /End of card-header --> --}}
 
                     <!-- Card body -->
                     <div class="card-body">
-                        <table id="example1" class="table table-responsive table-bordered table-striped">
+                        <table id="example1" class="table table-bordered table-striped">
                             <thead>
                                 <tr>
                                     <th>#</th>
@@ -133,6 +137,118 @@
             <!--/.col-12 -->
         </div>
         <!--/.row -->
+
+        <!-- create modal -->
+        <div class="modal fade" id="createNewItem" data-backdrop="static" data-keyboard="false"
+            tabindex="-1" aria-labelledby="createNewItem" aria-hidden="true">
+            <div class="modal-dialog">
+                <!-- Modal Content -->
+                <div class="modal-content">
+                    <!-- Modal Header -->
+                    <div class="modal-header">
+                        <!-- Modal Title -->
+                        <h5 class="modal-title" id="createNewItem">
+                            Add a new train
+                        </h5>
+
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <!-- End of Modal Header -->
+
+                    <!-- Form -->
+                    <form action="{{route('trains.store')}}" method="POST">
+                        @csrf
+                        <!-- Modal Body -->
+                        <div class="card-body">
+                            <!-- Name -->
+                            <div class="form-group">
+                                <label for="name">{{ trans('site.name') }}</label>
+                                <input type="text" class="form-control" id="name"
+                                    name="name" placeholder="{{trans('site.enter_name_train')}}"
+                                    required>
+                            </div>
+                            <!-- Train Type / Seats Count -->
+                            <div class="form-group">
+                                <div class="row">
+                                    <!-- Train Type -->
+                                    <div class="col">
+                                        <label for="train_type">{{ trans('site.train_type') }}</label>
+                                        <input type="text" class="form-control" id="train_type"
+                                            name="train_type" placeholder="{{trans('site.enter_name_type')}}"
+                                            required>
+                                    </div>
+                                    <!-- End of Train Type -->
+                                    <!-- Seats available -->
+                                    <div class="col">
+                                        <label for="seats_available">{{ trans('site.seats_available') }}</label>
+                                        <input type="number" class="form-control" id="seats_available"
+                                            name="seats_count" placeholder="{{trans('site.enter_seats_available_train')}}"
+                                            required>
+                                    </div>
+                                    <!-- End of Seats available -->
+                                </div>
+                            </div>
+                            <!-- Depature Station / Time -->
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col">
+                                        <label for="depature_station">{{ trans('site.depature_station') }}</label>
+                                        <!-- All stations -->
+                                        <select class="form-control select2 searchable" name="depature_station_id"
+                                            id="depature_station" required>
+                                            <option value="" >{{trans('site.all_stations')}}</option>
+                                            @foreach ($stations as $station)
+                                                <option value="{{$station->id}}">{{$station->name}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="col">
+                                        <label for="depature_at">{{ trans('site.depature_at') }}</label>
+                                        <input class="form-control" type="datetime-local" id="depature_at"
+                                            name="depature_at" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Arrival Station / Time -->
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col">
+                                        <label for="arrival_station">{{ trans('site.arrival_station') }}</label>
+                                        <!-- All stations -->
+                                        <select class="form-control select2 searchable" name="arrival_station_id"
+                                            id="arrival_station" required>
+                                            <option value="" >{{trans('site.all_stations')}}</option>
+                                            @foreach ($stations as $station)
+                                                <option value="{{$station->id}}" >{{$station->name}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="col">
+                                        <label for="arrival_at">{{ trans('site.arrival_at') }}</label>
+                                        <input class="form-control" type="datetime-local" id="arrival_at"
+                                            name="arrival_at" required>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End of Modal Body -->
+                        <!-- Modal Footer -->
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary btn-add btn-crayons">
+                                {{ trans('site.add') }}
+                            </button>
+                        </div>
+                        <!-- End of Card Footer -->
+                    </form>
+                    <!-- End of  Form -->
+                </div>
+                <!-- End of Modal Content -->
+            </div>
+        </div>
+        <!-- end of create modal -->
+
     </section>
 @endsection
 
