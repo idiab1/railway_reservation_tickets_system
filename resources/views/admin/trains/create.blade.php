fe95bc7cfd2ff245458d5802b0493f81e957151d
islamdiab-stack <islamdiab967@gmail.com>
2022-03-12 17:47:01 +0200

    change the layout for trains create page

--- resources/views/admin/trains/create.blade.php
+++ resources/views/admin/trains/create.blade.php
@@ -31,113 +31,135 @@
 
 {{-- Content --}}
 @section('content')
-<div class="trains-page">
+<section class="trains-section section">
     <div class="row">
-        <div class="col-md-8 m-auto">
-            <div class="trains-form">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">{{ trans('site.add_train') }}</h3>
+        <div class="col-md-12 m-auto">
+
+            <!-- form container -->
+            <div class="form-container">
+                <div class="row">
+                    <div class="col-md-7 p-0">
+                        <!-- Stations form -->
+                        <div class="trains-form section-form">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h3 class="card-title">{{ trans('site.add_train') }}</h3>
+                                </div>
+                                <!-- /.card-header -->
+                                    <!-- form start -->
+                                    <form action="{{route('trains.store')}}" method="POST">
+                                        @csrf
+                                        <div class="card-body">
+                                            <!-- Name -->
+                                            <div class="form-group">
+                                                <label for="name">{{ trans('site.name') }}</label>
+                                                <input type="text" class="form-control" id="name"
+                                                    name="name" placeholder="{{trans('site.enter_name_train')}}"
+                                                    required>
+                                            </div>
+
+                                            <!-- Train Type / Seats Count -->
+                                            <div class="form-group">
+                                                <div class="row">
+
+                                                    <!-- Train Type -->
+                                                    <div class="col">
+                                                        <label for="train_type">{{ trans('site.train_type') }}</label>
+                                                        <input type="text" class="form-control" id="train_type"
+                                                            name="train_type" placeholder="{{trans('site.enter_name_type')}}"
+                                                            required>
+                                                    </div>
+                                                    <!-- End of Train Type -->
+
+                                                    <!-- Seats available -->
+                                                    <div class="col">
+                                                        <label for="seats_available">{{ trans('site.seats_available') }}</label>
+                                                        <input type="number" class="form-control" id="seats_available"
+                                                            name="seats_count" placeholder="{{trans('site.enter_seats_available_train')}}"
+                                                            required>
+                                                    </div>
+                                                    <!-- End of Seats available -->
+                                                </div>
+                                            </div>
+
+                                            <!-- Depature Station / Time -->
+                                            <div class="form-group">
+                                                <div class="row">
+                                                    <div class="col">
+                                                        <label for="depature_station">{{ trans('site.depature_station') }}</label>
+                                                        <!-- All stations -->
+                                                        <select class="form-control select2 searchable" name="depature_station_id"
+                                                            id="depature_station" required>
+                                                            <option value="" >{{trans('site.all_stations')}}</option>
+                                                            @foreach ($stations as $station)
+                                                                <option value="{{$station->id}}">{{$station->name}}</option>
+                                                            @endforeach
+                                                        </select>
+                                                    </div>
+                                                    <div class="col">
+                                                        <label for="depature_at">{{ trans('site.depature_at') }}</label>
+                                                        <input class="form-control" type="datetime-local" id="depature_at"
+                                                            name="depature_at" required>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <!-- Arrival Station / Time -->
+                                            <div class="form-group">
+                                                <div class="row">
+                                                    <div class="col">
+                                                        <label for="arrival_station">{{ trans('site.arrival_station') }}</label>
+                                                        <!-- All stations -->
+                                                        <select class="form-control select2 searchable" name="arrival_station_id"
+                                                            id="arrival_station" required>
+                                                            <option value="" >{{trans('site.all_stations')}}</option>
+                                                            @foreach ($stations as $station)
+                                                                <option value="{{$station->id}}" >{{$station->name}}</option>
+                                                            @endforeach
+                                                        </select>
+                                                    </div>
+                                                    <div class="col">
+                                                        <label for="arrival_at">{{ trans('site.arrival_at') }}</label>
+                                                        <input class="form-control" type="datetime-local" id="arrival_at"
+                                                            name="arrival_at" required>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                        <!-- /.card-body -->
+
+                                        <!-- Card Footer -->
+                                        <div class="card-footer">
+                                            <button type="submit" class="btn btn-primary btn-add btn-crayons">
+                                                {{ trans('site.add') }}
+                                            </button>
+                                        </div>
+                                        <!-- End of Card Footer -->
+                                    </form>
+
+                                </div>
+                        <!-- /.card -->
+                        </div>
+                        <!-- End of Stations form -->
                     </div>
-                    <!-- /.card-header -->
-                        <!-- form start -->
-                        <form action="{{route('trains.store')}}" method="POST">
-                            @csrf
-                            <div class="card-body">
-                                <!-- Name -->
-                                <div class="form-group">
-                                    <label for="name">{{ trans('site.name') }}</label>
-                                    <input type="text" class="form-control" id="name"
-                                        name="name" placeholder="{{trans('site.enter_name_train')}}"
-                                        required>
-                                </div>
-
-                                <!-- Train Type / Seats Count -->
-                                <div class="form-group">
-                                    <div class="row">
-
-                                        <!-- Train Type -->
-                                        <div class="col">
-                                            <label for="train_type">{{ trans('site.train_type') }}</label>
-                                            <input type="text" class="form-control" id="train_type"
-                                                name="train_type" placeholder="{{trans('site.enter_name_type')}}"
-                                                required>
-                                        </div>
-                                        <!-- End of Train Type -->
-
-                                        <!-- Seats available -->
-                                        <div class="col">
-                                            <label for="seats_available">{{ trans('site.seats_available') }}</label>
-                                            <input type="number" class="form-control" id="seats_available"
-                                                name="seats_count" placeholder="{{trans('site.enter_seats_available_train')}}"
-                                                required>
-                                        </div>
-                                        <!-- End of Seats available -->
-                                    </div>
-                                </div>
-
-                                <!-- Depature Station / Time -->
-                                <div class="form-group">
-                                    <div class="row">
-                                        <div class="col">
-                                            <label for="depature_station">{{ trans('site.depature_station') }}</label>
-                                            <!-- All stations -->
-                                            <select class="form-control select2 searchable" name="depature_station_id"
-                                                id="depature_station" required>
-                                                <option value="" >{{trans('site.all_stations')}}</option>
-                                                @foreach ($stations as $station)
-                                                    <option value="{{$station->id}}">{{$station->name}}</option>
-                                                @endforeach
-                                            </select>
-                                        </div>
-                                        <div class="col">
-                                            <label for="depature_at">{{ trans('site.depature_at') }}</label>
-                                            <input class="form-control" type="datetime-local" id="depature_at"
-                                                name="depature_at" required>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <!-- Arrival Station / Time -->
-                                <div class="form-group">
-                                    <div class="row">
-                                        <div class="col">
-                                            <label for="arrival_station">{{ trans('site.arrival_station') }}</label>
-                                            <!-- All stations -->
-                                            <select class="form-control select2 searchable" name="arrival_station_id"
-                                                id="arrival_station" required>
-                                                <option value="" >{{trans('site.all_stations')}}</option>
-                                                @foreach ($stations as $station)
-                                                    <option value="{{$station->id}}" >{{$station->name}}</option>
-                                                @endforeach
-                                            </select>
-                                        </div>
-                                        <div class="col">
-                                            <label for="arrival_at">{{ trans('site.arrival_at') }}</label>
-                                            <input class="form-control" type="datetime-local" id="arrival_at"
-                                                name="arrival_at" required>
-                                        </div>
-                                    </div>
-                                </div>
-
+                    <div class="col-md-5 p-0">
+                        <!-- Stations Info -->
+                        <div class="station-info section-info">
+                            <div class="card">
+                                <div class="card-body"></div>
                             </div>
-                            <!-- /.card-body -->
-
-                            <!-- Card Footer -->
-                            <div class="card-footer">
-                                <button type="submit" class="btn btn-primary btn-add btn-crayons">
-                                    {{ trans('site.add') }}
-                                </button>
-                            </div>
-                            <!-- End of Card Footer -->
-                        </form>
-
+                        </div>
+                        <!-- End of Stations Info -->
                     </div>
-            <!-- /.card -->
+                </div>
             </div>
+            <!-- End of form container -->
+
+
         </div>
     </div>
-</div>
+</section>
 @endsection
 
 @section('scripts')
