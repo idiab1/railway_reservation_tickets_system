bf80cca1929424e18c06daed12e4af141d06d4f0
islamdiab-stack <islamdiab967@gmail.com>
2022-03-12 03:30:32 +0200

    change the layout for create page for users

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -18,10 +18,140 @@
 
 {{-- Content --}}
 @section('content')
-<div class="users-page">
+<section class="users-section section">
     <div class="row">
-        <div class="col-md-8 m-auto">
-            <div class="users-form">
+        <div class="col-md-12 m-auto">
+
+            <!-- form container -->
+            <div class="form-container">
+                <div class="row">
+                    <div class="col-md-6 p-0">
+                        <!-- users form -->
+                        <div class="users-form section-form">
+                            <!-- Card -->
+                            <div class="card">
+                                <!-- Card header -->
+                                <div class="card-header">
+                                    <h3 class="card-title">{{ trans('site.add_user') }}</h3>
+                                </div>
+                                <!-- End of card header -->
+                                <!-- form -->
+                                {{-- <form action="{{route('stations.store')}}" method="POST">
+                                    @csrf
+                                    <div class="card-body">
+                                        <!-- Name -->
+                                        <div class="form-group m-0">
+                                            <label for="name">{{ trans('site.name') }}</label>
+                                            <input class="form-control" type="text" id="name" name="name"
+                                                placeholder="{{trans('site.enter_name_station')}}"
+                                                required>
+                                        </div>
+
+                                    </div>
+                                    <!-- /.card-body -->
+
+                                    <div class="card-footer">
+                                        <button type="submit" class="btn btn-primary btn-add btn-crayons">{{ trans('site.add') }}</button>
+                                    </div>
+                                </form> --}}
+
+                                <form action="{{route('users.store')}}" method="POST">
+                                    @csrf
+                                    <div class="card-body">
+                                        <!-- Name -->
+                                        <div class="form-group">
+                                            <label for="name">{{ trans('site.name') }}</label>
+                                            <input class="form-control" type="text" id="name"
+                                                    name="name" placeholder="{{trans('site.admin_enter_name')}}">
+                                        </div>
+
+                                        <!-- Email -->
+                                        <div class="form-group">
+                                            <label for="email">{{ trans('site.email') }}</label>
+                                            <input class="form-control" type="email" id="email"
+                                                    name="email" placeholder="{{trans('site.admin_enter_email')}}">
+                                        </div>
+
+                                        <!-- Password -->
+                                        <div class="form-group">
+                                            <label for="password">{{ trans('site.password') }}</label>
+                                            <input class="form-control" type="password" id="password"
+                                                    name="password" placeholder="{{trans('site.admin_enter_password')}}">
+                                        </div>
+
+                                        <!-- Confirm password -->
+                                        <div class="form-group">
+                                            <label for="confirmPassword">{{ trans('site.confirm_password') }}</label>
+                                            <input class="form-control" type="password" id="confirmPassword"
+                                                    name="password_confirmation" placeholder="{{trans('site.admin_confirm_password')}}">
+                                        </div>
+
+                                        <!-- Privileges -->
+                                        <div class="form-group">
+                                            <label for="supervisors">{{ trans('site.privileges') }}</label>
+
+                                            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                                                @php
+                                                    $models = ['users', 'stations'];
+                                                    $maps = ['create', 'read', 'update', 'delete']
+                                                @endphp
+
+                                                @foreach ($models as $index => $model)
+                                                    <li class="nav-item">
+                                                        <a class="nav-link btn btn-sm {{$index == 0 ? "active" : ""}}"
+                                                        id="pills-{{$model}}-tab" data-toggle="pill" href="#pills-{{$model}}"
+                                                        role="tab" aria-controls="pills-{{$model}}">{{ trans('site.' . $model) }}</a>
+                                                    </li>
+
+                                                @endforeach
+
+                                            </ul>
+                                            <div class="tab-content" id="pills-tabContent">
+
+                                                @foreach ($models as $index => $model)
+
+                                                    <div class="tab-pane fade show {{$index == 0 ? "active" : ""}}" id="pills-{{$model}}" role="tabpanel" aria-labelledby="pills-{{$model}}-tab">
+
+                                                        @foreach ($maps as $map)
+                                                            <label><input type="checkbox" name="permissions[]" value="{{$model . '-' . $map}}">{{ trans('site.' . $map) }} </label>
+                                                        @endforeach
+
+                                                    </div>
+                                                @endforeach
+                                            </div>
+
+                                        </div>
+
+
+                                    </div>
+                                    <!-- /.card-body -->
+
+                                    <div class="card-footer">
+                                        <button type="submit" class="btn btn-primary btn-add btn-crayons">{{ trans('site.add') }}</button>
+                                    </div>
+                                </form>
+                                <!-- End of form -->
+
+                            </div>
+                            <!-- End of card -->
+                        </div>
+                        <!-- End of Stations form -->
+                    </div>
+                    <div class="col-md-6 p-0">
+                        <!-- user Info -->
+                        <div class="user-info section-info">
+                            <div class="card">
+                                <div class="card-body"></div>
+                            </div>
+                        </div>
+                        <!-- End of user Info -->
+                    </div>
+                </div>
+            </div>
+            <!-- End of form container -->
+
+
+            {{-- <div class="users-form">
                 <div class="card">
                     <div class="card-header">
                         <h3 class="card-title">{{ trans('site.add_user') }}</h3>
@@ -106,10 +236,10 @@
 
                     </div>
             <!-- /.card -->
-            </div>
+            </div> --}}
         </div>
     </div>
-</div>
+</section>
 @endsection
 
 @section('scripts')
