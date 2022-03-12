ff4a77f92f9b9d2b7cecb40a5afaa035dcf74461
islamdiab-stack <islamdiab967@gmail.com>
2022-03-12 17:15:54 +0200

    write style for admin homepage

--- resources/views/admin/adminHome.blade.php
+++ resources/views/admin/adminHome.blade.php
@@ -5,6 +5,22 @@
     Home
 @endsection
 
+{{-- Styles --}}
+@section('styles')
+
+<style>
+    .content-header{
+        height: 100px;
+    }
+    .content-header .content-header-info {
+        text-align: center;
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
+
+@endsection
+
 {{-- Page name --}}
 @section('page_name')
     Control Panel
