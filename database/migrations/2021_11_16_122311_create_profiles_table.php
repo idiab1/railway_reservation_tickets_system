faecef8ecbbe0f8c3d7e868b09db8416ccc84b0d
islamdiab-stack <islamdiab967@gmail.com>
2022-03-18 01:06:22 +0200

    add two column for age, and gender

--- database/migrations/2021_11_16_122311_create_profiles_table.php
+++ database/migrations/2021_11_16_122311_create_profiles_table.php
@@ -16,11 +16,13 @@ class CreateProfilesTable extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
-            $table->string('image')->nullable()->default('default.png');
             $table->string('facebook')->nullable();
             $table->string('twitter')->nullable();
             $table->string('linkedin')->nullable();
+            $table->integer('age')->nullable();
             $table->text('about')->nullable();
+            $table->string('image')->nullable()->default('default.png');
+            $table->enum('gender', ['male', 'female'])->nullable()->default("male");
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
