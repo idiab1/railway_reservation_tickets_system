3a40c6eff7796ff476a418c39bff0124c625c7f4
islamdiab-stack <islamdiab967@gmail.com>
2022-03-18 05:06:16 +0200

    remove margin form last of form group

--- resources/views/admin/stations/index.blade.php
+++ resources/views/admin/stations/index.blade.php
@@ -125,7 +125,7 @@
                         <!-- Modal Body -->
                         <div class="modal-body">
                             <!-- Name -->
-                            <div class="form-group">
+                            <div class="form-group m-0">
                                 <label for="name">{{ trans('site.name') }}</label>
                                 <input class="form-control" type="text" id="name"
                                     name="name" placeholder="{{trans('site.enter_name_station')}}">
