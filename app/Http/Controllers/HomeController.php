7cec2b274af76dbca8d805b6a872bd320ed708f7
islamdiab-stack <islamdiab967@gmail.com>
2022-03-20 03:47:40 +0200

    get all stations from database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Station;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,7 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $stations = Station::all();
+        return view('home', compact("stations"));
     }
 
 }
