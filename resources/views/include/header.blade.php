1bddabf6caaed3ac6d2eece01545edfb9f67f64b
islamdiab-stack <islamdiab967@gmail.com>
2022-03-21 06:04:24 +0200

    use yield for header info in main header

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,25 +1,15 @@
 <!-- Header -->
-<header class="main-header">
+<header class="main-header sub-header">
     <!-- Header Box -->
     <div class="header-box">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
-                    <!-- Header info -->
-                    <div class="header-info">
-                        <p>Train Service</p>
-                        <h1>Welcome to Egyption Railway</h1>
-                        <p>We saves your time both while burchasing, the check-in and during the travel </p>
-                    </div>
-                    <!-- End of Header info -->
+                    @yield('header-info')
                 </div>
             </div>
-
-
         </div>
     </div>
     <!-- End of Header Box -->
-
-
 </header>
 <!-- End of header -->
