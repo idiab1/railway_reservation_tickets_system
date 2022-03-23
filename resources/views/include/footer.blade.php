3457a27c9b2c1b9738d20eba67a9348442d27713
islamdiab-stack <islamdiab967@gmail.com>
2022-03-23 08:46:50 +0200

    update

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -6,21 +6,21 @@
                 <!-- Copyright -->
                 <div class="copyright">
                     <div class="row">
-                        <div class="col-md-5 col-sm-12">
+                        <div class="col-md-6 col-sm-12">
                             <!-- Logo Brand -->
                             <div class="logo-brand">
                                 <h6 class="brand-name">{{$setting->web_name}}</h6>
                             </div>
                             <!-- ./end of logo Brand -->
                         </div>
-                        <div class="col-md-2 col-sm-12 d-none d-md-block">
+                        {{-- <div class="col-md-2 col-sm-12 d-none d-md-block">
                             <!-- Footer Icon -->
                             <div class="footer-icon">
                                 <i class="fas fa-arrow-circle-up"></i>
                             </div>
                             <!-- End of Footer Icon -->
-                        </div>
-                        <div class="col-md-5 col-sm-12">
+                        </div> --}}
+                        <div class="col-md-6 col-sm-12">
                             <div class="copyright-info text-right">
                                 <p>&copy; 2022 {{ trans('site.copyright') }}</p>
                             </div>
