296d0b337ac528df8c3043f507b4402a40c7d3f3
islamdiab-stack <islamdiab967@gmail.com>
2022-03-23 08:58:37 +0200

    check if header component exist then calculate minim height for section

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,4 +1,6 @@
 // Adjust calculate minimum height for section
+// Get height of header
+const headerH = document.querySelector(".main-header").clientHeight;
 // Select on section element
 const section = document.querySelector(".section");
 // Get height of footer
@@ -6,3 +8,7 @@ let footerH = document.querySelector(".main-footer").clientHeight;
 
 section.style.minHeight = ((window.innerHeight) - ( footerH +  navbarH)) + "px";
 
+if(headerH){
+    section.style.minHeight = ((window.innerHeight) - ( navbarH + headerH + footerH )) + "px";
+    console.log(headerH)
+}
