97ec734c78eee15118223108c1a38b425b4e10dd
islamdiab-stack <islamdiab967@gmail.com>
2022-03-31 14:08:58 +0200

    check if page has main header

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,13 +1,13 @@
 // Adjust calculate minimum height for section
-// Get height of header
-const headerH = document.querySelector(".main-header").clientHeight;
 // Select on section element
 const section = document.querySelector(".section");
 // Get height of footer
 let footerH = document.querySelector(".main-footer").clientHeight;
 
-section.style.minHeight = ((window.innerHeight) - ( footerH +  navbarH)) + "px";
+section.style.minHeight = (window.innerHeight) - ( footerH +  navbarH) + "px";
 
-if(headerH){
+if(document.querySelector(".main-header")){
+    // Get height of header
+    const headerH = document.querySelector(".main-header").clientHeight;
     section.style.minHeight = ((window.innerHeight) - ( navbarH + headerH + footerH )) + "px";
 }
