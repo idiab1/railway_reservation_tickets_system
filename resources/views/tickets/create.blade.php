1f72a530cb8d968bd2201da64286743046023813
islamdiab-stack <islamdiab967@gmail.com>
2022-04-01 14:52:22 +0200

    create a new page for reserve tickets

--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -7,89 +7,15 @@
 
 {{-- Styles --}}
 @section('other-styles')
+    <link rel="stylesheet" href="{{asset("plugins/stepper/css/bs-stepper.min.css")}}">
     <style>
-        .sub-header {
-            height: 270px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }
-
-        .sub-header .header-box .header-info {
-            width: auto;
-            margin-top: 0;
-            text-align: center;
-        }
-
-        .tickets-content .ticket{
-            flex-direction: row;
-        }
-
-        .tickets-content .ticket .card-header{
-            border: none;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-            background-color: transparent;
-            border-right: 1px solid rgba(0, 0, 0, 0.125);
-            padding: 0.75rem 1rem;
-        }
-
-        .tickets-content .ticket .card-header .icon {
-            background-color: #98354e;
-            width: 40px;
-            height: 40px;
-            color: #eee6ce;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            border-radius: 50%;
-        }
-
-        .tickets-content .ticket .card-header .icon i{
-            font-size: 20px;
-        }
-
-        .tickets-content .ticket .card-header p{
-            color: #98354e;
-            font-weight: bold;
-            padding-top: 5px;
-            text-transform: capitalize;
-        }
-
-        .tickets-content .ticket .card-body{
-            background-color: transparent;
-            padding: 0.75rem 1rem;
-        }
-
-        .tickets-content .ticket .card-body .ticket-details{
-            display: flex;
-            justify-content: space-between;
-        }
-
-        .tickets-content .ticket .ticket-price{
-            padding-top: 10px;
-        }
-        .tickets-content .ticket .card-body .ticket-details .icon{
-            display: flex;
-            align-items: center;
-        }
-
-        .tickets-content .ticket .card-footer{
-            border: none;
-            display: flex;
-            padding: 0.75rem 1rem;
-            justify-content: center;
-            align-items: center;
-            background-color: transparent;
-            border-left: 1px solid rgba(0, 0, 0, 0.125);
-        }
 
     </style>
 @endsection
 
 {{-- Header --}}
+@section('header') @endsection
+
 @section('header-info')
     <!-- Header info -->
     <div class="header-info">
@@ -101,10 +27,85 @@
 @section('content')
 <!-- Tickets Section -->
 <section class="tickets-section section">
-
     <!-- Container fluid -->
     <div class="container">
 
+        <div class="`p-4 bg-white shadow-sm">
+            <!-- Stepper -->
+            <div id="stepper1" class="bs-stepper">
+                <!-- Stepper Header -->
+                <div class="bs-stepper-header" role="tablist">
+                    <!-- Step Item -->
+                    <div class="step" data-target="#test-l-1">
+                        <button type="button" class="step-trigger" role="tab"
+                            id="stepper1trigger1" aria-controls="test-l-1">
+                            <span class="bs-stepper-circle">1</span>
+                            <span class="bs-stepper-label">Email</span>
+                        </button>
+                    </div>
+                    <!-- End of Step Item -->
+
+                    <div class="bs-stepper-line"></div>
+
+                    <!-- Step Item -->
+                    <div class="step" data-target="#test-l-2">
+                        <button type="button" class="step-trigger" role="tab"
+                            id="stepper1trigger3" aria-controls="test-l-2">
+                            <span class="bs-stepper-circle">2</span>
+                            <span class="bs-stepper-label">Validate</span>
+                        </button>
+                    </div>
+                    <!-- End of Step Item -->
+                </div>
+                <!-- End of Stepper Header -->
+
+                <!-- Stepper Content -->
+                <div class="bs-stepper-content">
+                    <form onSubmit="return false" method="POST" action="{{route("user.tickets.store")}}">
+                        @csrf
+                        <!-- Stepper one -->
+                        <div id="test-l-1" role="tabpanel" class="bs-stepper-pane"
+                            aria-labelledby="stepper1trigger1">
+                            <!-- Email Address -->
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input type="email" class="form-control" id="username"
+                                    placeholder="Enter email" required>
+                            </div>
+                            <!-- Email Address -->
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                            </div>
+                            <!-- Email Address -->
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                            </div>
+                            <!-- Email Address -->
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                            </div>
+                            <button class="btn btn-primary crayons-btn crayons-btn" onclick="stepper.next()">Next</button>
+                        </div>
+                        <!-- End of Stepper one -->
+
+                        <!-- Stepper three -->
+                        <div id="test-l-2" role="tabpanel" class="bs-stepper-pane text-center"
+                            aria-labelledby="stepper1trigger3">
+                            <button class="btn btn-primary crayons-btn mt-5" onclick="stepper.previous()">Previous</button>
+                            <button type="submit" class="btn btn-primary crayons-btn mt-5">Submit</button>
+                        </div>
+                        <!-- Stepper three -->
+
+                    </form>
+                </div>
+                <!-- End of Stepper Content -->
+
+            </div>
+            <!-- End of stepper -->
+        </div>
     </div>
     <!-- End of Container fluid -->
 </section>
@@ -116,5 +117,23 @@
 <script src="{{asset('js/main.js')}}"></script>
 <!--Custom script -->
 <script src="{{asset('js/custom.js')}}"></script>
+
+<script src="{{asset('plugins/stepper/js/bs-stepper.min.js')}}"></script>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        let stepper1 = document.querySelector('.bs-stepper');
+        window.stepper = new Stepper(document.querySelector('.bs-stepper'))
+    });
+
+//   myStepper = new Stepper(document.querySelector('#stepper1'),{
+//     linear: false
+// })
+
+// myStepper = new Stepper(document.querySelector('#stepper1'),{
+//   animation: true
+// })
+
+</script>
 
 @endsection
