d6315abcc343ea708fcc2865ef44574e62e16f94
islamdiab-stack <islamdiab967@gmail.com>
2022-04-13 11:08:52 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ReservationController;
-use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
