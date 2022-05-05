463817acfcb6d39c8e7416a5caa34f2e8538e3ea
islamdiab-stack <islamdiab967@gmail.com>
2022-05-05 03:52:52 +0300

    update

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -83,7 +83,7 @@
 
         <!-- Main content -->
         <section class="content">
-            <div class="container">
+            <div class="container-fluid">
                 @yield('content')
             </div>
         </section>
