7ba7f11f0c92ceadbc8f52640b0267410a285576
islamdiab-stack <islamdiab967@gmail.com>
2022-05-05 04:19:53 +0300

    create a new component for reservations

--- resources/views/admin/adminHome.blade.php
+++ resources/views/admin/adminHome.blade.php
@@ -7,6 +7,11 @@
 
 {{-- Styles --}}
 @section('styles')
+<!-- DataTables -->
+<link rel="stylesheet" href="{{asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+<link rel="stylesheet" href="{{asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+<link rel="stylesheet" href="{{asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
+
 
 <style>
     .content-header{
@@ -16,6 +21,15 @@
         text-align: center;
         display: flex;
         justify-content: space-between;
+    }
+    .dataTables_length{
+        float: right;
+        margin-bottom: 12px;
+    }
+    .card h2.header{
+        color: #b8405e;
+        font-weight: 600;
+        font-size: 1.35rem;
     }
 </style>
 
@@ -32,7 +46,7 @@
     <section class="homepage">
 
         <!-- Status Content -->
-        <div class="status-content">
+        <div class="status-content pb-4">
             <div class="row">
                 <div class="col-12 col-sm-6 col-md-4">
                     <!-- Project box -->
@@ -88,6 +102,105 @@
         </div>
         <!-- End of Status Content -->
 
+        <div class="row">
+            <div class="col-12 col-md-5">
+                <div class="card">
+                    <!-- Card body -->
+                    <div class="card-body">
+                        <h4 class="header">{{ trans('site.reservations') }}</h4>
+                        <div class="">
+                            <div class="row buttons">
+                                <div class="col-sm-12 col-md-5">
+                                </div>
+                                <div class="col-sm-12 col-md-7 ">
+
+                                </div>
+                            </div>
+                        </div>
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>{{ trans('site.name') }}</th>
+                                    <th>{{ trans('site.date') }}</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @php
+                                    $id = 1;
+                                @endphp
+                                @if ($reservations->count() > 0)
+                                    @foreach ($reservations as $train)
+                                        <tr>
+                                            <td>{{$id++}}</td>
+                                            <!-- Name of train -->
+                                            <td>{{$train->name}}</td>
+
+                                            <td>
+                                                <button class="btn btn-danger btn-sm btn-show" type="button">
+                                                    <i class="fas fa-list"></i>
+                                                    {{ trans('site.show') }}
+                                                </button>
+
+                                                <!-- Delete button -->
+                                                <form class="d-inline-block" action="{{route('reservations.destroy', ['id' => $train->id])}}" method="POST">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button class="btn btn-danger btn-sm btn-delete" type="submit">
+                                                        <i class="fas fa-trash"></i>
+                                                        {{ trans('site.delete') }}
+                                                    </button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @endif
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th>#</th>
+                                    <th>{{ trans('site.name') }}</th>
+                                    <th>{{ trans('site.date') }}</th>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                    <!-- /end of card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+        </div>
+
+
+
     </section>
     <!-- End of Homepage -->
+@endsection
+
+{{-- Scripts --}}
+@section('scripts')
+<!-- DataTables  & Plugins -->
+<script src="{{asset('plugins/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{asset('plugins/jszip/jszip.min.js')}}"></script>
+<script src="{{asset('plugins/pdfmake/pdfmake.min.js')}}"></script>
+<script src="{{asset('plugins/pdfmake/vfs_fonts.js')}}"></script>
+<script src="{{asset('plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
+
+<script>
+    $(function () {
+        $("#example1").DataTable({
+            "filter": false,
+        })
+        $('.header').appendTo("#example1_wrapper .col-md-6:eq(0)");
+        $('.dataTables_length').appendTo("#example1_wrapper .col-md-6:eq(1)");
+
+    });
+</script>
+
 @endsection
