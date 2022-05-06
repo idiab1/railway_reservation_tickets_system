007ccdb0cab9803a16e0c7029af12aab13e86063
ESSO <islamdiab967@gmail.com>
2022-05-06 07:43:34 +0200

    update

--- public/js/types.js
+++ public/js/types.js
@@ -1,6 +1,3 @@
-window.addEventListener("load", () => {
-    document.querySelector(".all-types").click();
-})
 
 let typeItems = document.querySelectorAll(".type-item");
 typeItems.forEach(typeItem => {
@@ -58,50 +55,54 @@ typeItems.forEach(typeItem => {
         e.target.setAttribute("disabled", "")
         e.target.setAttribute("checked", "")
 
-        
+        console.log(e.target);
 
         
     })
 });
 
+// Click on all types when window loaded
+window.addEventListener("load", () => {
+    document.querySelector(".all-types").click();
+})
+
 
 document.querySelector(".all-types").addEventListener("click", (e) => {
-        e.preventDefault();
+    e.preventDefault();
 
-            // Get data-url
-            let url = document.querySelector(".all-types").getAttribute("data-url");
-            let method = document.querySelector(".all-types").getAttribute("data-method");
+    // Get data-url
+    let url = document.querySelector(".all-types").getAttribute("data-url");
+    let method = document.querySelector(".all-types").getAttribute("data-method");
 
-            // Change loading spinner to flex
-            document.querySelector(".loading").style.display = "flex";
+    // Change loading spinner to flex
+    document.querySelector(".loading").style.display = "flex";
 
 
-            $.ajax({
-                url: url,
-                method: method,
-                success: function(data){
-                    // console.log(data)
+    $.ajax({
+        url: url,
+        method: method,
+        success: function(data){
+            // console.log(data)
 
-                    // Display none on loading element
-                    document.querySelector(".loading").style.display = "none";
-                    // Clear data of all elements
-                    document.querySelector(".tickets-content-list").innerHTML = "";
+            // Display none on loading element
+            document.querySelector(".loading").style.display = "none";
+            // Clear data of all elements
+            document.querySelector(".tickets-content-list").innerHTML = "";
 
-                    document.querySelector(".tickets-content-list").insertAdjacentHTML("beforeend", data)
+            document.querySelector(".tickets-content-list").insertAdjacentHTML("beforeend", data)
 
-                }
-            })
-
-            let elmsChecked = document.querySelectorAll("input[type='checkbox']");
-        
-        elmsChecked.forEach(el => {
-            el.removeAttribute("disabled");
-            el.removeAttribute("checked");
-        })
-
-        // e.target.classList.add("active")
-        e.target.setAttribute("disabled", "")
-        e.target.setAttribute("checked", "")
-            
-
+        }
     })
+
+    let elmsChecked = document.querySelectorAll("input[type='checkbox']");
+        
+    elmsChecked.forEach(el => {
+        el.removeAttribute("disabled");
+        el.removeAttribute("checked");
+    })
+
+    // e.target.classList.add("active")
+    e.target.setAttribute("disabled", "")
+    e.target.setAttribute("checked", "")
+    
+})
