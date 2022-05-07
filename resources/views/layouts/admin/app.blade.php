451758437740e4284b83280d3edd6b9a84ea0f5c
ESSO <islamdiab967@gmail.com>
2022-05-07 13:20:20 +0200

    add meta for csrf tocken

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="_token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Unknown page') | {{ trans('site.dashboard') }}</title>
 
     <!-- Google Font: Source Sans Pro -->
