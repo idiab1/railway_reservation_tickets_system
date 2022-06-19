fb3b394eef0a3bce88cf333f1833970de6981203
ESSO <islamdiab967@gmail.com>
2022-06-19 04:13:50 +0200

    create a new file for translate all keywords

---
+++ resources/lang/ar/site.php
@@ -0,0 +1,167 @@
+<?php
+
+return [
+
+    // Links
+    'home' => 'الصفحة الرئيسية',
+    'users' => 'المستخدمين',
+    "passengers" => "المسافرين",
+    'messages' => 'الرسائل',
+    'setting' => 'الإعدادات',
+    'contact_us' => 'Contact Us',
+    'about_us' => 'About Us',
+    'news' => 'الاخبار',
+    'dashboard' => 'لوحة التحكم',
+    'stations' => 'المحطات',
+    'trains' => 'القطارات',
+    'types' => 'الانواع',
+    'seats' => 'المقاعد',
+    'tickets' => 'التذاكر',
+    'posts' => 'المقالات',
+    "profile" => "الصفحة الشخصية",
+    "tickets_reservations" => "التذاكر / الحجوزات",
+    'system_settings'=> 'إعدادات النظام',
+    'logout' => 'تسجيل خروج',
+    "control_panel" => "لوحة التحكم",
+
+    // Users Page
+    'list_users' => 'كل المستخدمين',
+    'add_user' => 'اضافة مستخدم جديد',
+    "user" => "المستخدم",
+    "users_registrations" => "",
+
+    // Passengers Page
+    'list_passengers' => 'كل المسافرين',
+    'add_passenger' => 'اضافة مسافر جديد',
+    "passenger" => "المسافر",
+
+    // Stations Page
+    'list_stations' => 'كل المحطات',
+    'add_station' => 'اضافة محطة جديدة',
+    'all_stations' => 'كل المحطات',
+    'station' => "المحطة",
+
+    // Trains Page
+    'list_trains' => 'كل القطارات',
+    'add_train' => 'اضافة قطار جديد',
+    'train' => "القطار",
+    'train_price' => "سعر التذكرة",
+
+    // Types Page
+    'all_types' => 'كل الأنواع',
+    'list_types' => 'كل الأنواع',
+    'add_type' => 'اضافة نوع قطار جديد',
+
+    // Posts Page
+    'list_posts' => 'كل المقالات',
+    'add_post' => 'اضافة مقال جديد',
+    'post' => "المقال",
+
+
+    // Ticket Page
+    "tickets" => "التذاكر",
+    "ticket" => 'التذكرة',
+
+    // Reservations
+    "reservations" => "الحجوزات",
+    "list_reservations" => "كل الحجوزات",
+
+
+
+    // Messages Page
+    'message' => 'الرسالة',
+    'list_all_messages' => 'كل الرسايل',
+
+
+
+
+    // Facades
+    'name' => 'اسم',
+    'action' => 'حدث',
+    "date" => "تاريخ",
+    'login' => 'تسجيل دخول',
+    'register' => 'إنشاء حساب جديد',
+    'email' => 'البريد الإلكتروني',
+    'password' => 'كلمة السر',
+    'confirm_password' => 'تأكيد كلمة السر',
+    'privileges' => 'Privileges',
+    'remember_me' => 'تذكرني',
+    'forgot_password' => 'هل نسيت كلمة السر',
+    'reset_password' => 'Reset Password',
+    'send_password_link' => 'Send Password Reset Link',
+    'menu' => 'Menu',
+    'about' => 'About',
+    'depature_at' => 'وقت المغادرة',
+    'arrival_at' => 'وقت الوصول',
+    'depature_station' => 'محطة المغادرة',
+    'arrival_station' => 'محطة الوصول',
+    'seats_available' => 'المقاعد المتاحة',
+    'class_price' => 'سعر الفئة',
+    'seats_count' => 'عدد المقاعد',
+    'currency' => 'EGP',
+    'website_name' => 'إسم الموقع',
+    'address' => 'العنوان',
+    'phone_number' => 'رقم الهاتف',
+    'created_at' => 'Created At',
+    'content' => 'المحتوي',
+    'train_type' => "نوع القطار",
+    "overview" => "نظرة عامة",
+    "image" => "الصورة",
+    "change_logo" => "Change Logo",
+    "setting_info" => "Setting Info",
+    "to" => "إلي",
+    "from" => "من",
+    "all" => "كل",
+    "user_type" => "نوع المستخدم",
+    "admin" => "مسئول",
+    "super_admin" => "مسئول",
+    "moderator" => "مشرف",
+    "waiting" => "انتظر",
+    "more_info" => "المزيد",
+
+    // Buttons
+    'send' => 'ارسال',
+    'show' => 'عرض',
+    'delete' => 'حذف',
+    'edit' => 'تعديل',
+    'create' => 'إنشاء',
+    'read' => 'قراءة',
+    'update' => 'تحديث',
+    'add' => 'إضافة',
+    'add_new' => "Add new",
+
+    // Languages
+    'english' => 'انجليزي',
+    'arabic' => 'عربي',
+
+    // Placeholder
+    'search' => 'بحث',
+    'enter_name' => 'Enter your Name',
+    'enter_email' => 'Enter your email address',
+    'enter_your_message' => 'Type your message here',
+    'enter_password' => 'Enter Password',
+    'admin_enter_name' => 'Enter name of user',
+    'admin_enter_email' => 'Enter email of user',
+    'admin_enter_password' => 'Enter password of user',
+    'admin_confirm_password' => 'Confirm password',
+    'admin_about_here' => 'About Here',
+    'enter_name_station' => 'Enter name of station',
+    'enter_name_train' => 'Enter name of train',
+    'enter_depature_station' => 'Enter depature station',
+    'enter_arrival_station' => 'Enter arrival station',
+    'enter_name_type' => 'Enter name of type',
+    'enter_website_name' => 'Enter Name of website',
+    'enter_phone_number' => 'Enter phone number of website',
+    'enter_website_email' => 'Enter Email of website',
+    'enter_address' => 'Enter address',
+    'enter_name_post' => 'Enter name of post',
+    'enter_content_post' => 'Enter Content of post',
+    'enter_seats_available_train' => "Enter count of seats available",
+    'enter_train_price' => "Enter price of ticket train",
+    'enter_train_type' => "Enter type of train",
+
+    // Footer
+    'copyright' => 'Copyright all rights reserved.',
+
+
+];
