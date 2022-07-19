92fa403134af361ae34ab9e719bcd3eff3595447
islamdiab-stack <islamdiab967@gmail.com>
2022-07-19 03:30:41 +0300

    change the layout where create a post info

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -18,61 +18,68 @@
 
 {{-- Content --}}
 @section('content')
-<div class="posts-page">
+<section class="post-section section">
     <div class="row">
-        <div class="col-md-9 m-auto">
-            <div class="posts-form">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">{{ trans('site.add_post') }}</h3>
-                    </div>
-                    <!-- /.card-header -->
-                        <!-- form start -->
-                        <form action="{{route('posts.store')}}" method="POST">
-                            @csrf
-                            <div class="card-body">
-                                <!-- Name -->
-                                <div class="form-group">
-                                    <label for="name">{{ trans('site.name') }}</label>
-                                    <input class="form-control" type="text" id="name"
-                                            name="name" placeholder="{{trans('site.enter_name_post')}}">
+        <div class="col-md-8 m-auto">
+            <!-- Form Container -->
+            <div class="form-container">
+                <div class="row">
+                    <div class="col-md-7 p-0">
+                        <!-- posts form -->
+                        <div class="posts-form section-form">
+                            <!-- Card -->
+                            <div class="card">
+                                <!-- Card Header -->
+                                <div class="card-header">
+                                    <h3 class="card-title">{{ trans('site.add_post') }}</h3>
                                 </div>
+                                <!-- /.card-header -->
+                                <!-- form start -->
+                                <form action="{{route('posts.store')}}" method="POST">
+                                    @csrf
+                                    <div class="card-body">
+                                        <!-- Name -->
+                                        <div class="form-group">
+                                            <label for="name">{{ trans('site.name') }}</label>
+                                            <input class="form-control" type="text" id="name"
+                                                name="name" placeholder="{{trans('site.enter_name_post')}}">
+                                        </div>
 
-                                <!-- Content -->
-                                <div class="form-group">
-                                    <label for="content">{{ trans('site.content') }}</label>
-                                    <textarea class="form-control ckeditor" name="content" id="content"
-                                            placeholder="{{trans('site.enter_content_post')}}"></textarea>
-                                </div>
+                                        <!-- Content -->
+                                        <div class="form-group">
+                                            <label for="content">{{ trans('site.content') }}</label>
+                                            <textarea class="form-control" name="content" id="content"
+                                                    placeholder="{{trans('site.enter_content_post')}}"></textarea>
+                                        </div>
 
+                                    </div>
+                                    <!-- /.card-body -->
+
+                                    <div class="card-footer">
+                                        <button type="submit" class="btn btn-primary btn-add btn-crayons">
+                                            {{ trans('site.add') }}
+                                        </button>
+                                    </div>
+                                </form>
+                                <!-- ./form -->
                             </div>
-                            <!-- /.card-body -->
-
-                            <div class="card-footer">
-                                <button type="submit" class="btn btn-primary btn-add btn-crayons">
-                                    {{ trans('site.add') }}
-                                </button>
-                            </div>
-                        </form>
-
+                            <!-- End of card -->
+                        </div>
+                        <!-- End of posts form -->
                     </div>
-            <!-- /.card -->
+                    <div class="col-md-5 p-0">
+                        <!-- posts Info -->
+                        <div class="station-info section-info">
+                            <div class="card">
+                                <div class="card-body"></div>
+                            </div>
+                        </div>
+                        <!-- End of posts Info -->
+                    </div>
+                </div>
             </div>
+            <!-- End of Form Container -->
         </div>
     </div>
-</div>
-@endsection
-
-@section('scripts')
-
-<!--ckeditor standard-->
-<script src="{{ asset('plugins/ckeditor/ckeditor.js') }}"></script>
-
-<script>
-    $(document).ready(function(){
-        CKEDITOR.config.language = "{{app()->getLocale()}}";
-
-        // CKEDITOR.replace('content')
-    });
-</script>
+</section>
 @endsection
